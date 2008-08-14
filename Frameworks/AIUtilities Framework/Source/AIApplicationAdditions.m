b86cbb734df7611d35e53b4c6acf70d22c1238e8
David Smith <David Smith@localhost>
2008-08-14 06:20:45 +0000

    Remove Tiger-specific stuff from AIUtils

--- Frameworks/AIUtilities Framework/Source/AIApplicationAdditions.m
+++ Frameworks/AIUtilities Framework/Source/AIApplicationAdditions.m
@@ -9,32 +9,6 @@
 
 @implementation NSApplication (AIApplicationAdditions)
 
-//Make sure the version number defines exist; when compiling in 10.3, NSAppKitVersionNumber10_3 isn't defined 
-#ifndef NSAppKitVersionNumber10_3 
-#define NSAppKitVersionNumber10_3 743 
-#endif 
-	 	 
-//Make sure the version number defines exist; when compiling in 10.4, NSAppKitVersionNumber10_4 isn't defined 
-#ifndef NSAppKitVersionNumber10_4 
-#define NSAppKitVersionNumber10_4 824 
-#endif
-
-
-- (BOOL)isOnTigerOrBetter
-{
-	return (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_3);
-}
-
-- (BOOL)isOnLeopardOrBetter
-{
-	return (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_4);
-}
-
--(BOOL)isTiger
-{
-	return (floor(NSAppKitVersionNumber) == NSAppKitVersionNumber10_4);
-}
-
 - (BOOL)isWebKitAvailable
 {
 	static BOOL _initialized = NO;
--- Frameworks/AIUtilities Framework/Source/AISystemNetworkDefaults.m
+++ Frameworks/AIUtilities Framework/Source/AISystemNetworkDefaults.m
@@ -112,25 +112,23 @@
 			}
 
 		} else {
-			if ([NSApp isOnTigerOrBetter]) {
-				//Check for a PAC configuration
-				enable = [[proxyDict objectForKey:(NSString *)kSCPropNetProxiesProxyAutoConfigEnable] boolValue];
-				if (enable) {
-					NSString *pacFile = [proxyDict objectForKey:(NSString *)kSCPropNetProxiesProxyAutoConfigURLString];
-					
-					if (pacFile) {
-						//XXX can't use pac file
-						NSString *msg = [NSString stringWithFormat:
-							AILocalizedString(@"The systemwide proxy configuration specified via the Network System Preferences depends upon reading a PAC (Proxy Automatic Confiruation) file from %@.  This information can not be used at this time; to connect, please obtain proxy information from your network administrator and use it manually.", nil),
-							pacFile];
-						NSRunCriticalAlertPanel(AILocalizedString(@"Unable to read proxy information", "Title of the alert shown when the system proxy configuration can not be determined"),
-												msg,
-												nil,
-												nil,
-												nil);
-					}
+			//Check for a PAC configuration
+			enable = [[proxyDict objectForKey:(NSString *)kSCPropNetProxiesProxyAutoConfigEnable] boolValue];
+			if (enable) {
+				NSString *pacFile = [proxyDict objectForKey:(NSString *)kSCPropNetProxiesProxyAutoConfigURLString];
+				
+				if (pacFile) {
+					//XXX can't use pac file
+					NSString *msg = [NSString stringWithFormat:
+						AILocalizedString(@"The systemwide proxy configuration specified via the Network System Preferences depends upon reading a PAC (Proxy Automatic Confiruation) file from %@.  This information can not be used at this time; to connect, please obtain proxy information from your network administrator and use it manually.", nil),
+						pacFile];
+					NSRunCriticalAlertPanel(AILocalizedString(@"Unable to read proxy information", "Title of the alert shown when the system proxy configuration can not be determined"),
+											msg,
+											nil,
+											nil,
+											nil);
 				}
-			}		
+			}
 		}
 		// Could check and process kSCPropNetProxiesExceptionsList here, which returns: CFArray[CFString]
 
