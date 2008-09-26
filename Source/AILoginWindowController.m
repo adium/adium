be140816f594099cddd939025a54664f0bf388a9
Peter Hosey <git@boredzo.org>
2008-09-26 14:07:00 +0000

    For consistency for the rest of the source code, use `#ifdef` here, not `#if`. Refs #11146.

--- Source/AILoginWindowController.m
+++ Source/AILoginWindowController.m
@@ -115,7 +115,7 @@
     [loginDict setObject:[NSNumber numberWithBool:[checkbox_displayOnStartup state]] forKey:LOGIN_SHOW_WINDOW];
 
     //Save the login they used
-#if DEBUG_BUILD
+#ifdef DEBUG_BUILD
     [loginDict setObject:selectedUserName forKey:LOGIN_LAST_USER_DEBUG];
 #else
     [loginDict setObject:selectedUserName forKey:LOGIN_LAST_USER];
