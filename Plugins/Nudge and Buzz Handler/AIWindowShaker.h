19793da9623261039cb86eda7e042ae6ba717774
David Smith <catfish.man@gmail.com>
2008-11-20 06:33:30 +0000

    Remove unnecessary imports of cocoa/appkit/foundation

--- Plugins/Nudge and Buzz Handler/AIWindowShaker.h
+++ Plugins/Nudge and Buzz Handler/AIWindowShaker.h
@@ -1,6 +1,3 @@
-#import <Cocoa/Cocoa.h>
-
-
 @interface AIWindowShaker : AIPlugin {
 
 }
--- Plugins/Purple Service/AMPurpleJabberAdHocServer.h
+++ Plugins/Purple Service/AMPurpleJabberAdHocServer.h
@@ -1,5 +1,3 @@
-#import <Cocoa/Cocoa.h>
-
 #import "ESPurpleJabberAccount.h"
 
 @class AMPurpleJabberAdHocCommand;
