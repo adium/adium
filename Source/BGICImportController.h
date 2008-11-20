8b8c7d12610f675fdc623d1cd0971c88e6291158
David Smith <David Smith@localhost>
2008-11-20 06:57:50 +0000

    Eliminate Adium.h and its one use

--- Source/BGICImportController.h
+++ Source/BGICImportController.h
@@ -14,8 +14,9 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#import <Adium/Adium.h>
 #import "SetupWizardBackgroundView.h"
+#import <Adium/AIWindowController.h>
+#import <Adium/AIAccount.h>
 
 @class BGICLogImportController, AWBonjourService;
 
