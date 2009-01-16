4ee1ace0c72be20288ecaa9eb5dc9cdce89f5764
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2009-01-16 23:15:09 +0000

    Fix copyright

--- Plugins/Purple Service/AIPurpleMobileMeAccountViewController.h
+++ Plugins/Purple Service/AIPurpleMobileMeAccountViewController.h
@@ -3,7 +3,7 @@
 //  Adium
 //
 //  Created by Evan Schoenberg on 7/16/08.
-//  Copyright 2008 Adium X / Saltatory Software. All rights reserved.
+//  Copyright 2008 Adium X. All rights reserved.
 //
 
 #import "AIPurpleOscarAccountViewController.h"
