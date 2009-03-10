51e1fa78929c31f789f70531b2466cd6e0e774fb
Zachary West <zacwest@gmail.com>
2009-03-10 00:03:23 +0000

    Fix the one pixel line at the top of the contact list when size to fit vertically is not enabled in standard contact lists. The window height was +1 the view height, and the view was 1px too low.

--- Frameworks/Adium Framework/Source/AIAbstractListController.m
+++ Frameworks/Adium Framework/Source/AIAbstractListController.m
@@ -430,12 +430,7 @@ static NSString *AIWebURLsWithTitlesPboardType = @"WebURLsWithTitlesPboardType";
 	theFont = [[prefDict objectForKey:KEY_LIST_LAYOUT_GROUP_FONT] representedFont];
 	[groupCell setFont:(theFont ? theFont : GROUP_FONT_IF_FONT_NOT_FOUND)];
 
-	//Standard special cases, add necessary spacing.
-	if (windowStyle == AIContactListWindowStyleStandard) {
-		[contactListView setDesiredHeightPadding:2];
-	} else {
-		[contactListView setDesiredHeightPadding:1];
-	}
+	[contactListView setDesiredHeightPadding:1];
 	
 	//Bubbles special cases
 	pillowsOrPillowsFittedWindowStyle = (windowStyle == AIContactListWindowStyleContactBubbles || windowStyle == AIContactListWindowStyleContactBubbles_Fitted);
