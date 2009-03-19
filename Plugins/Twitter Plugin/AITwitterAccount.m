f4abb0cf33de006dc68800ad7ab3f86ff0f61de8
Zachary West <zacw@adiumx.com>
2009-03-19 15:17:18 +0000

    Only toggle the follows-in-clist when online.

--- Plugins/Twitter Plugin/AITwitterAccount.m
+++ Plugins/Twitter Plugin/AITwitterAccount.m
@@ -868,7 +868,7 @@
 		updateAfterSend = [[prefDict objectForKey:TWITTER_PREFERENCE_UPDATE_AFTER_SEND] boolValue];
 		retweetLink = [[prefDict objectForKey:TWITTER_PREFERENCE_RETWEET_SPAM] boolValue];
 		
-		if ([key isEqualToString:TWITTER_PREFERENCE_LOAD_CONTACTS]) {
+		if ([key isEqualToString:TWITTER_PREFERENCE_LOAD_CONTACTS] && self.online) {
 			if ([[prefDict objectForKey:TWITTER_PREFERENCE_LOAD_CONTACTS] boolValue]) {
 				// Delay updates when loading our contacts list.
 				[self silenceAllContactUpdatesForInterval:18.0];
