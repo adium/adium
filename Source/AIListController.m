8b7bd396f7386c739da6d2c4a7ab9a0405cee0e8
David Smith <David Smith@localhost>
2009-03-19 21:27:46 +0000

    Noob less. Thanks Zac

--- Source/AIListController.m
+++ Source/AIListController.m
@@ -434,13 +434,18 @@
 {	
 	//Don't update for objects not on this list (groupchat occupants mostly)
 	AIListObject *obj = [notification object];
+	BOOL onList = NO;
 	if ([obj isKindOfClass:[AIListContact class]]) {
 		for (AIListGroup *group in ((AIListContact *)obj).parentContact.groups) {
-			if (group.contactList == self.contactList)
+			if (group.contactList == self.contactList) {
+				onList = YES;
 				break;
+			}
 		}
-		return;
 	}
+	
+	if (!onList)
+		return;
 		
 	[super listObjectAttributesChanged:notification];
 	
