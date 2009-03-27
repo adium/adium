72946600ed17719acf5aeaeaf6cf56346fdb81e6
Colin Barrett <Colin Barrett@localhost>
2009-03-27 07:34:24 +0000

    upload-nightly.sh: Using scp(1) properly is a prerequisite for it working correctly

--- Release/upload-nightly.sh
+++ Release/upload-nightly.sh
@@ -9,4 +9,4 @@ PARENT=$(dirname $0)
 cd ${PARENT:-.}
 
 ADIUM_RELEASE_NAME=`head -n 1 build/latest | tail -n 1`
-scp build/latest build/${ADIUM_RELEASE_NAME}.tgz ${NIGHTLY_USER}@${NIGHTLY_HOST}
+scp build/latest build/${ADIUM_RELEASE_NAME}.tgz ${NIGHTLY_USER}@${NIGHTLY_HOST}:
