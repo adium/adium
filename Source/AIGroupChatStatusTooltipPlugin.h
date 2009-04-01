3411c7c9e1180c60ddd3e93024f8405d71deaf44
Zachary West <zacw@adiumx.com>
2009-04-01 01:47:11 +0000

    Add a tooltip plugin which displays the chats we have in common with a contact, their status in the chat, and their status icon in the chat.

---
+++ Source/AIGroupChatStatusTooltipPlugin.h
@@ -0,0 +1,15 @@
+//
+//  AIGroupChatStatusTooltipPlugin.h
+//  Adium
+//
+//  Created by Zachary West on 2009-03-31.
+//
+
+#import <Adium/AIPlugin.h>
+#import <Adium/AIInterfaceControllerProtocol.h>
+
+@interface AIGroupChatStatusTooltipPlugin : AIPlugin <AIContactListTooltipEntry> {
+
+}
+
+@end
