1614753df01aa1c218c82021616c79234613ec02
Zachary West <zacwest@gmail.com>
2009-04-04 03:11:26 +0000

    Pesky little NSLog() that slipped in.

--- Source/AIContactController.m
+++ Source/AIContactController.m
@@ -941,8 +941,6 @@ NSInteger contactDisplayNameSort(AIListObject *objectA, AIListObject *objectB, v
 - (NSSet *)updateListObject:(AIListObject *)inObject keys:(NSSet *)inModifiedKeys silent:(BOOL)silent
 {
 	if ((!inModifiedKeys || [inModifiedKeys containsObject:@"Online"]) && [inObject isKindOfClass:[AIListContact class]]) {
-		NSLog(@"Restoring %@", ((AIListContact *)inObject).parentContact);
-		
 		[((AIListContact *)inObject).parentContact restoreGrouping];
 	}
 	
