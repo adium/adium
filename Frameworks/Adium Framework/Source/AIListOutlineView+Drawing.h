dea85c2432286abfc8560cb11fc1419d9d7b1968
Evan Schoenberg <Evan Schoenberg@localhost>
2009-04-09 22:28:39 +0000

    Moved the drawing related methods in AIListOutlineView to a category, "AIListOutlineView_Drawing".  This makes AIListOutlineView's main implementation a lot easier to deal with.

---
+++ Frameworks/Adium Framework/Source/AIListOutlineView+Drawing.h
@@ -0,0 +1,10 @@
+//
+//  AIListOutlineView+Drawing.h
+//  Adium
+//
+//  Created by Evan Schoenberg on 4/9/09.
+//  Copyright 2009 Adium X / Saltatory Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/AIListOutlineView.h>
