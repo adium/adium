f05182a20283a8a56c0ab29d12d68927d47adf7a
David Smith <catfish.man@gmail.com>
2009-04-22 00:20:48 -0700

    Add everything necessary to block quit until async writes are done except the actual blocking. NSOperationQueue has API for this, RAOperationQueue doesn't

--- Frameworks/AIUtilities Framework/Source/AISharedWriterQueue.m
+++ Frameworks/AIUtilities Framework/Source/AISharedWriterQueue.m
@@ -14,8 +14,21 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 #import "AISharedWriterQueue.h"
+#import "RAOperationQueue.h"
+
+@interface AISharedWriterQueue()
++ (RAOperationQueue *)queue;
+@end
 
 @implementation AISharedWriterQueue
+
++ (void) addOperation:(RAOperation *)op {
+	[[self queue] addOperation:op];
+}
+
++ (void) waitUntilAllOperationsAreFinished {
+#warning FIXME need a way to do this on RAOperationQueue (or switch to NSOperationQueue)
+}
 
 + (RAOperationQueue *)queue {
 	NSAssert([NSThread currentThread] == [NSThread mainThread], @"Do not try to use AISharedWriterQueue from non-main threads");
