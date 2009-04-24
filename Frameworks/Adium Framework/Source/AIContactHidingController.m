f52d07ba042968723bf3ab7527b906b7e637b042
Zachary West <zacw@adiumx.com>
2009-04-24 16:36:44 -0400

    Consider "always visible" after a search check, not before.

--- Frameworks/Adium Framework/Source/AIContactHidingController.m
+++ Frameworks/Adium Framework/Source/AIContactHidingController.m
@@ -135,10 +135,6 @@ static AIContactHidingController *sharedControllerInstance = nil;
 	if (![container containsObject:listObject])
 		return NO;
 	
-	// Don't do any processing for a contact that's always visible.
-	if (listObject.alwaysVisible)
-		return YES;
-	
 	if ([listObject conformsToProtocol:@protocol(AIContainingObject)]) {
 		// A meta contact must meet the criteria for a contact to be visible and also have at least 1 contained contact
 		return ([(id<AIContainingObject>)listObject visibleCount] > 0 || ([listObject boolValueForProperty:@"New Object"] &&
@@ -151,6 +147,10 @@ static AIContactHidingController *sharedControllerInstance = nil;
 			return [matched boolValue];
 		return [self evaluatePredicateOnListObject:listObject withSearchString:searchString];
 	}
+	
+	// Don't do any processing for a contact that's always visible.
+	if (listObject.alwaysVisible)
+		return YES;
 	
 	if ([listObject isKindOfClass:[AIListBookmark class]])
 		return ((AIListBookmark *)listObject).account.online;
