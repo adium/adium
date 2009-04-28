e1c4b0d2f036f80ff1fc185145f8b05463f74e56
Colin Barrett <colin@springsandstruts.com>
2009-04-28 02:49:55 -0700

    Add tooltips for closed groups. Fixes #11951

--- Source/AIListObjectContentsPlugin.m
+++ Source/AIListObjectContentsPlugin.m
@@ -14,28 +14,36 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#import "ESMetaContactContentsPlugin.h"
+#import "AIListObjectContentsPlugin.h"
 #import <Adium/AIContactControllerProtocol.h>
 #import <Adium/AIInterfaceControllerProtocol.h>
 #import <Adium/AIMenuControllerProtocol.h>
 #import <Adium/AIListContact.h>
 #import <Adium/AIListObject.h>
+#import <Adium/AIListGroup.h>
 #import <Adium/AIMetaContact.h>
 #import <Adium/AIAbstractListController.h>
 #import <Adium/AIServiceIcons.h>
 #import <AIUtilities/AIImageDrawingAdditions.h>
 #import <AIUtilities/AIMenuAdditions.h>
 #import <Adium/AIStatusIcons.h>
+#import <Adium/AIContactHidingController.h>
+#import <Adium/AISortController.h>
 
 #define META_TOOLTIP_ICON_SIZE NSMakeSize(11,11)
 
 #define EXPAND_CONTACT		AILocalizedString(@"Expand Combined Contact", nil)
 #define COLLAPSE_CONTACT	AILocalizedString(@"Collapse Combined Contact", nil)
+
+#define MAX_CONTACTS 20
+#define MORE_CONTACTS_STRING AILocalizedString(@"%d others", @"Used to describe omitted contacts.\
+                                                               The first parameter is the number of omitted contacts")
+
 /*!
- * @class ESMetaContactContentsPlugin
+ * @class AIListObjectContentsPlugin
  * @brief Tooltip component: Show the contacts contained by metaContacts, with service and status state.
  */
-@implementation ESMetaContactContentsPlugin
+@implementation AIListObjectContentsPlugin
 
 /*!
  * @brief Install
@@ -72,7 +80,7 @@
  */
 - (NSString *)labelForObject:(AIListObject *)inObject
 {
-	if ([inObject isKindOfClass:[AIMetaContact class]]) {
+	if ([inObject conformsToProtocol:@protocol(AIContainingObject)]) {
 		return AILocalizedString(@"Contacts",nil);
 	}
 	
@@ -88,20 +96,25 @@
 {
     NSMutableAttributedString	*entry = nil;
 	
-	if ([inObject isKindOfClass:[AIMetaContact class]]) {
-		NSArray				*listContacts = [(AIMetaContact *)inObject uniqueContainedObjects];
+	if ([inObject conformsToProtocol:@protocol(AIContainingObject)]) {
+        id<AIContainingObject> containingObject = (id<AIContainingObject>)inObject;
+		NSArray *listContacts = [[containingObject uniqueContainedObjects] sortedArrayUsingActiveSortControllerInContainer:containingObject];
 		
-		//Only display the contents if it has more than one contact within it.
-		if ([listContacts count] > 1) {
-			NSMutableString	*entryString;
-			AIListContact	*contact;
+		//Only display the contents of a meta if it has more than one contact within it.
+		if ([inObject isKindOfClass:[AIListGroup class]] || [listContacts count] > 1) {
 			BOOL			shouldAppendString = NO;
-			BOOL			shouldAppendServiceIcon = ![(AIMetaContact *)inObject containsOnlyOneService];
+			BOOL			shouldAppendServiceIcon = ([inObject isKindOfClass:[AIMetaContact class]] && ![(AIMetaContact *)inObject containsOnlyOneService]);
 
 			entry = [[NSMutableAttributedString alloc] init];
-			entryString = [entry mutableString];
+            NSMutableString	*entryString = [entry mutableString];
 			
-			for (contact in listContacts) {
+            NSUInteger count = 0;
+			for (AIListContact *contact in listContacts) {
+                if ([inObject isKindOfClass:[AIListGroup class]] && 
+                    ![[AIContactHidingController sharedController] visibilityOfListObject:contact inContainer:containingObject]) {
+                    continue;
+                }
+                                
 				NSImage	*statusIcon, *serviceIcon;
 				
 				if (shouldAppendString) {
@@ -110,6 +123,12 @@
 					shouldAppendString = YES;
 				}
 				
+                // If there are a lot of contacts, just stop.
+                if (++count >= MAX_CONTACTS) {
+                    [entryString appendString:[NSString stringWithFormat:MORE_CONTACTS_STRING, listContacts.count - MAX_CONTACTS]];
+                    break;
+                }                
+                
 				statusIcon = [[AIStatusIcons statusIconForListObject:contact
 																type:AIStatusIconTab
 														   direction:AIIconNormal] imageByScalingToSize:META_TOOLTIP_ICON_SIZE];
@@ -131,7 +150,11 @@
 					[entryString appendString:@" "];
 				}
 				
-				[entryString appendString:contact.formattedUID];
+                if ([inObject isKindOfClass:[AIMetaContact class]]) {
+                    [entryString appendString:contact.formattedUID];
+                } else if ([inObject isKindOfClass:[AIListGroup class]]) {
+                    [entryString appendString:contact.displayName];
+                }
 				
 				if (shouldAppendServiceIcon) {
 					serviceIcon = [[AIServiceIcons serviceIconForObject:contact type:AIServiceIconSmall direction:AIIconNormal]
