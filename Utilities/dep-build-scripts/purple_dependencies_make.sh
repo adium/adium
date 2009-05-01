7f04d2b4cc09c5d4cbb258554a0d0190e9bd3745
David Smith <catfish.man@gmail.com>
2009-04-30 20:32:46 -0700

    Initial shot at 64 bit deps. Hooray for discovering --with-libiconv=native :)

--- Utilities/dep-build-scripts/purple_dependencies_make.sh
+++ Utilities/dep-build-scripts/purple_dependencies_make.sh
@@ -23,7 +23,7 @@ pushd "$SOURCEDIR/$MEANWHILE" > /dev/null 2>&1
     done
 popd > /dev/null 2>&1
 
-for ARCH in ppc i386 ; do
+for ARCH in ppc i386 x86_64 ; do
 	echo "Building Meanwhile for $ARCH"
 
     export CFLAGS="$BASE_CFLAGS -arch $ARCH"
@@ -34,8 +34,11 @@ for ARCH in ppc i386 ; do
 			 export PATH="$PATH_PPC"
 			 export PKG_CONFIG_PATH="$TARGET_DIR_PPC/lib/pkgconfig";;
 		i386) TARGET_DIR="$TARGET_DIR_I386"
-			  export PATH="$PATH_I386"
-			  export PKG_CONFIG_PATH="$TARGET_DIR_I386/lib/pkgconfig";;
+			 export PATH="$PATH_I386"
+			 export PKG_CONFIG_PATH="$TARGET_DIR_I386/lib/pkgconfig";;
+		x86_64) TARGET_DIR="$TARGET_DIR_X86_64"
+			 export PATH="$PATH_X86_64"
+			 export PKG_CONFIG_PATH="$TARGET_DIR_X86_64/lib/pkgconfig";;
 	esac
 
     mkdir meanwhile-$ARCH || true
@@ -68,7 +71,7 @@ pushd "$SOURCEDIR/$MEANWHILE" > /dev/null 2>&1
 popd > /dev/null 2>&1
 
 # Gadu-gadu
-for ARCH in ppc i386 ; do
+for ARCH in ppc i386 x86_64 ; do
 	echo "Building Gadu-Gadu for $ARCH"
 
 	export CFLAGS="$BASE_CFLAGS -arch $ARCH"
@@ -84,6 +87,10 @@ for ARCH in ppc i386 ; do
   		      TARGET_DIR="$TARGET_DIR_I386"
 			  export PATH="$PATH_I386"
 			  export PKG_CONFIG_PATH="$TARGET_DIR_I386/lib/pkgconfig";;
+		x86_64) HOST=x86_64-apple-darwin9
+			 TARGET_DIR="$TARGET_DIR_X86_64"
+			 export PATH="$PATH_X86_64"
+			 export PKG_CONFIG_PATH="$TARGET_DIR_X86_64/lib/pkgconfig";;
 	esac
 	
 	mkdir gadu-$ARCH || true
@@ -102,7 +109,7 @@ done
 
 # intltool so pidgin will configure
 # need a native intltool in both ppc and i386
-for ARCH in ppc i386 ; do
+for ARCH in ppc i386 x86_64 ; do
 	echo "Building intltool for $ARCH"
 
     mkdir intltool-$ARCH || true
@@ -111,6 +118,7 @@ for ARCH in ppc i386 ; do
     case $ARCH in
         ppc) TARGET_DIR="$TARGET_DIR_PPC" ;;
         i386) TARGET_DIR="$TARGET_DIR_I386" ;;
+        x86_64) TARGET_DIR="$TARGET_DIR_X86_64" ;;
     esac
 
 	echo '  Configuring...'   
