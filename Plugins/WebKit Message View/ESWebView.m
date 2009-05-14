7ff154e2e1a0f6a9e45828cfb65baf69f3c33c90
David Smith <catfish.man@gmail.com>
2009-05-14 13:46:57 -0700

    Bad override, no cookie. Fixes #12029

--- Plugins/WebKit Message View/ESWebView.m
+++ Plugins/WebKit Message View/ESWebView.m
@@ -63,6 +63,7 @@
 		[win setOpaque:!transparentBackground];
 		[win _setContentHasShadow:NO];
 	}
+	[super viewDidMoveToWindow];
 }
 
 //Font Family ----------------------------------------------------------------------------------------------------------
