01bebebb67e88a0d8ee11f27f32584a38f31351c
Zachary West <zacw@adium.im>
2009-05-16 20:56:30 -0400

    Didn't hit save before checking in the collapsed-count removal.

--- Frameworks/Adium Framework/Source/AIListGroupBubbleToFitCell.m
+++ Frameworks/Adium Framework/Source/AIListGroupBubbleToFitCell.m
@@ -28,7 +28,7 @@
 {
 	NSString *countText;
 
-	if (([listObject boolValueForProperty:@"Show Count"] || (showCollapsedCount && ![controlView isItemExpanded:proxyObject])) &&
+	if ([listObject boolValueForProperty:@"Show Count"] &&
 		(countText = [listObject valueForProperty:@"Count Text"])) {
 		return [[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@ (%@)", [self labelString], countText]
 												attributes:[self labelAttributes]] autorelease];
@@ -47,7 +47,7 @@
 {
 	int width = [super cellWidth];
 
-	if (([listObject boolValueForProperty:@"Show Count"] || (showCollapsedCount && ![controlView isItemExpanded:proxyObject])) && 
+	if ([listObject boolValueForProperty:@"Show Count"] && 
 		[listObject valueForProperty:@"Count Text"]) {
 		//We'll be added a space and parenthesis to the group count if it's displayed
 		NSAttributedString *countText = [[NSAttributedString alloc] initWithString:@" ()"
