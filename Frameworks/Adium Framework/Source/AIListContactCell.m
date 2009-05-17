5d5deab9b987419e4abc7102559af1e466678957
David Smith <catfish.man@gmail.com>
2009-05-16 19:05:09 -0700

    warnings--; apparently we wanted to keep this around, but I see no reason to have it spamming the output

--- Frameworks/Adium Framework/Source/AIListContactCell.m
+++ Frameworks/Adium Framework/Source/AIListContactCell.m
@@ -669,7 +669,7 @@
 - (NSColor *)textColor
 {
 	NSColor	*theTextColor;
-	BOOL	isEvent = [listObject boolValueForProperty:@"Is Event"];
+	//BOOL	isEvent = [listObject boolValueForProperty:@"Is Event"];
 	/* XXX If it's an event, we may want to be inheriting from more than just the metacontact's preferred contact...
 	 * this is the only case for that which I've come across */
 	if (shouldUseContactTextColors && (theTextColor = [listObject valueForProperty:@"Text Color"])) {
