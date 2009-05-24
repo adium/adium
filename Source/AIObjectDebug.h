379ac9cd278b2bc1bd4f4046fc2f5b55ce7444e2
David Smith <catfish.man@gmail.com>
2009-05-24 01:06:52 -0700

    Sending an object a selector it doesn't recognize currently breaks Adium in mysterious and unpredictable ways by unwinding the stack out from under code that doesn't expect it. Instead, let's just crash so we can get a stack trace and fix it.

---
+++ Source/AIObjectDebug.h
@@ -0,0 +1,13 @@
+//
+//  AIStringDebug.h
+//  Adium
+//
+//  Created by David Smith on 5/24/2009
+//  Copyright 2009 The Adium Team. All rights reserved.
+//
+
+@interface AIObjectDebug : NSObject {
+
+}
+
+@end
