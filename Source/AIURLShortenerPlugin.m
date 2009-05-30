a54d4c46921cdc5504121db7fbec6055e5d3418f
Zachary West <zacw@adium.im>
2009-05-30 17:07:29 -0400

    Add a bit.ly service for URL shortening.
    This currently uses an "adiumofficial" account as the account it's tied to. History is not enabled (history=1 would have to be appended to the link), so other than being a stupid requirement, users won't be able to claim this URL. :/ I'm not writing a UI for setting the username/API key so whatever.

--- Source/AIURLShortenerPlugin.m
+++ Source/AIURLShortenerPlugin.m
@@ -105,6 +105,7 @@
 																		  [NSNumber numberWithInteger:AIisgd], @"is.gd",
 																		  [NSNumber numberWithInteger:AIMetamark], @"xrl.us",
 																		  [NSNumber numberWithInteger:AITrim], @"tr.im",
+																		  [NSNumber numberWithInteger:AIBitly], @"bit.ly",
 																		  nil];
 
 	[menu removeAllItems];
@@ -234,19 +235,23 @@
 	
 	switch(service) {
 		case AITinyURL:
-			request = [NSString stringWithFormat:@"http://tinyurl.com/api-create.php?url=%@", [address stringByEncodingURLEscapes]];
+			request = [NSString stringWithFormat:@"http://tinyurl.com/api-create.php?url=%@", [address stringByAddingPercentEscapesForAllCharacters]];
 			break;
 			
 		case AIisgd:
-			request = [NSString stringWithFormat:@"http://is.gd/api.php?longurl=%@", [address stringByEncodingURLEscapes]];
+			request = [NSString stringWithFormat:@"http://is.gd/api.php?longurl=%@", [address stringByAddingPercentEscapesForAllCharacters]];
 			break;
 			
 		case AIMetamark:
-			request = [NSString stringWithFormat:@"http://metamark.net/api/rest/simple?long_url=%@", [address stringByEncodingURLEscapes]];
+			request = [NSString stringWithFormat:@"http://metamark.net/api/rest/simple?long_url=%@", [address stringByAddingPercentEscapesForAllCharacters]];
 			break;
 			
 		case AITrim:
-			request = [NSString stringWithFormat:@"http://api.tr.im/api/trim_simple?api_key=zghQN6sv5y0FkLPNlQAopm7qDQz6ItO33ENU21OBsy3dL1Kl&url=%@", [address stringByEncodingURLEscapes]];
+			request = [NSString stringWithFormat:@"http://api.tr.im/api/trim_simple?api_key=zghQN6sv5y0FkLPNlQAopm7qDQz6ItO33ENU21OBsy3dL1Kl&url=%@", [address stringByAddingPercentEscapesForAllCharacters]];
+			break;
+			
+		case AIBitly:
+			request = [NSString stringWithFormat:@"http://api.bit.ly/shorten?version=2.0.1&format=text&longUrl=%@&login=adiumofficial&apiKey=R_3a9f4affdf9181536d5536f67e02a59c", [address stringByAddingPercentEscapesForAllCharacters]];
 			break;
 			
 		default:
