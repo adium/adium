5ce6596d6e6592a84fe5bec489e44c955aa6f6d6
Zachary West <zacw@adium.im>
2009-07-10 22:52:39 -0400

    Abstract multipart image uploading a bit and add an imageshack.us service for image uploading.

--- Plugins/Image Uploading Plugin/AIPicImImageUploader.m
+++ Plugins/Image Uploading Plugin/AIPicImImageUploader.m
@@ -7,24 +7,7 @@
 //
 
 #import "AIPicImImageUploader.h"
-
-#import <Adium/AIChat.h>
-#import <Adium/AIInterfaceControllerProtocol.h>
 #import <AIUtilities/AIStringAdditions.h>
-#import <AIUtilities/AIProgressDataUploader.h>
-#import <AIUtilities/AIImageAdditions.h>
-
-#define MULTIPART_FORM_BOUNDARY	@"bf5faadd239c17e35f91e6dafe1d2f96"
-#define PIC_IM_URL				@"http://api.tr.im/api/picim_url.xml?api_key=zghQN6sv5y0FkLPNlQAopm7qDQz6ItO33ENU21OBsy3dL1Kl"
-#define PIC_IM_MAX_SIZE			2500000
-
-@interface AIPicImImageUploader()
-- (id)initWithImage:(NSImage *)inImage
-		   uploader:(AIImageUploaderPlugin *)inUploader
-			   chat:(AIChat *)inChat;
-- (void)uploadImage;
-- (void)parseResponse:(NSData *)data;
-@end
 
 @implementation AIPicImImageUploader
 + (NSString *)serviceName
@@ -32,107 +15,28 @@
 	return @"pic.im";
 }
 
-+ (id)uploadImage:(NSImage *)image forUploader:(AIImageUploaderPlugin *)uploader inChat:(AIChat *)chat;
+- (NSString *)uploadURL
 {
-	return [[[self alloc] initWithImage:image uploader:uploader chat:chat] autorelease];
+	return @"http://api.tr.im/api/picim_url.xml?api_key=zghQN6sv5y0FkLPNlQAopm7qDQz6ItO33ENU21OBsy3dL1Kl";
 }
 
-- (id)initWithImage:(NSImage *)inImage
-		   uploader:(AIImageUploaderPlugin *)inUploader
-			   chat:(AIChat *)inChat
+- (NSString *)fieldName
 {
-	if ((self = [super init])) {
-		image = [inImage retain];
-		uploader = inUploader;
-		chat = inChat;
-		
-		[self uploadImage];
-	}
-	
-	return self;
+	return @"media";
+}
+
+- (NSUInteger)maximumSize
+{
+	return 2500000;
 }
 
 - (void)dealloc
 {
-	[dataUploader release]; dataUploader = nil;
 	[resultData release]; resultData = nil;
 	[response release]; response = nil;
 	[responseParser release]; responseParser = nil;
-	[image release]; image = nil;
 	
 	[super dealloc];
-}
-
-#pragma mark Data uploader delegate
-- (void)updateUploadProgress:(NSUInteger)uploaded total:(NSUInteger)total context:(id)context
-{
-	[uploader updateProgress:uploaded total:total forChat:chat];
-}
-
-- (void)uploadCompleted:(id)context result:(NSData *)result
-{
-	if (result.length) {
-		[self parseResponse:result];
-	} else {
-		[uploader errorWithMessage:AILocalizedString(@"Unable to upload", nil) forChat:chat];
-	}
-}
-
-- (void)uploadFailed:(id)context
-{
-	[uploader errorWithMessage:AILocalizedString(@"Unable to upload", nil) forChat:chat];
-}
-
-#pragma mark Image upload
-
-- (void)uploadImage
-{
-	NSMutableData *body = [NSMutableData data];
-	
-	NSBitmapImageFileType bestType;
-	
-	NSData *pngRepresentation = [[image largestBitmapImageRep] representationUsingType:NSPNGFileType properties:nil];
-	NSData *jpgRepresentation = [[image largestBitmapImageRep] representationUsingType:NSJPEGFileType properties:nil];
-	NSData *imageRepresentation;
-	
-	if (pngRepresentation.length > jpgRepresentation.length) {
-		bestType = NSJPEGFileType;
-		imageRepresentation = jpgRepresentation;
-	} else {
-		bestType = NSPNGFileType;
-		imageRepresentation = pngRepresentation;
-	}
-	
-	if (imageRepresentation.length > PIC_IM_MAX_SIZE) {
-		imageRepresentation = [image representationWithFileType:bestType maximumFileSize:PIC_IM_MAX_SIZE];
-	}
-	
-	if (!imageRepresentation) {
-		[uploader errorWithMessage:AILocalizedString(@"Unable to upload", nil) forChat:chat];
-		return;
-	}
-	
-	[body appendData:[[NSString stringWithFormat:@"--%@\r\n", MULTIPART_FORM_BOUNDARY] dataUsingEncoding:NSUTF8StringEncoding]];
-	[body appendData:[@"Content-Disposition: form-data; name=\"media\"; filename=\"image.png\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-	[body appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n\r\n", (bestType == NSJPEGFileType) ? @"image/jpeg" : @"image/png"] dataUsingEncoding:NSUTF8StringEncoding]];
-	[body appendData:imageRepresentation];
-	[body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", MULTIPART_FORM_BOUNDARY] dataUsingEncoding:NSUTF8StringEncoding]];
-
-	NSDictionary *headers = [NSDictionary dictionaryWithObjectsAndKeys:
-							 [NSString stringWithFormat:@"multipart/form-data; boundary=%@", MULTIPART_FORM_BOUNDARY], @"Content-type", nil];
-	
-	dataUploader = [[AIProgressDataUploader dataUploaderWithData:body
-															 URL:[NSURL URLWithString:PIC_IM_URL]
-														 headers:headers
-														delegate:self
-														 context:nil] retain];
-	
-	[dataUploader upload];
-}
-
-- (void)cancel
-{
-	[dataUploader cancel];
 }
 
 #pragma mark Response parsing
