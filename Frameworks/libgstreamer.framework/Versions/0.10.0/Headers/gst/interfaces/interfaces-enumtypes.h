c0258d29192847bba5a02603475ba8d25f83e222
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2009-08-06 21:25:11 -0500

    libgstreamer 0.10.24, gst-plugins-base 0.10.23, gst-plugins-good 0.10.15, and gst-plugins-bad 0.10.13

---
+++ Frameworks/libgstreamer.framework/Versions/0.10.0/Headers/gst/interfaces/interfaces-enumtypes.h
@@ -0,0 +1,45 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef __GST_INTERFACES_ENUM_TYPES_H__
+#define __GST_INTERFACES_ENUM_TYPES_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/* enumerations from "/Users/evands/adium/Utilities/dep-build-scripts/source/gst-plugins-base-0.10.23/gst-libs/gst/interfaces/colorbalance.h" */
+GType gst_color_balance_type_get_type (void);
+#define GST_TYPE_COLOR_BALANCE_TYPE (gst_color_balance_type_get_type())
+
+/* enumerations from "/Users/evands/adium/Utilities/dep-build-scripts/source/gst-plugins-base-0.10.23/gst-libs/gst/interfaces/mixer.h" */
+GType gst_mixer_type_get_type (void);
+#define GST_TYPE_MIXER_TYPE (gst_mixer_type_get_type())
+GType gst_mixer_message_type_get_type (void);
+#define GST_TYPE_MIXER_MESSAGE_TYPE (gst_mixer_message_type_get_type())
+GType gst_mixer_flags_get_type (void);
+#define GST_TYPE_MIXER_FLAGS (gst_mixer_flags_get_type())
+
+/* enumerations from "/Users/evands/adium/Utilities/dep-build-scripts/source/gst-plugins-base-0.10.23/gst-libs/gst/interfaces/mixertrack.h" */
+GType gst_mixer_track_flags_get_type (void);
+#define GST_TYPE_MIXER_TRACK_FLAGS (gst_mixer_track_flags_get_type())
+
+/* enumerations from "/Users/evands/adium/Utilities/dep-build-scripts/source/gst-plugins-base-0.10.23/gst-libs/gst/interfaces/navigation.h" */
+GType gst_navigation_command_get_type (void);
+#define GST_TYPE_NAVIGATION_COMMAND (gst_navigation_command_get_type())
+GType gst_navigation_query_type_get_type (void);
+#define GST_TYPE_NAVIGATION_QUERY_TYPE (gst_navigation_query_type_get_type())
+GType gst_navigation_message_type_get_type (void);
+#define GST_TYPE_NAVIGATION_MESSAGE_TYPE (gst_navigation_message_type_get_type())
+GType gst_navigation_event_type_get_type (void);
+#define GST_TYPE_NAVIGATION_EVENT_TYPE (gst_navigation_event_type_get_type())
+
+/* enumerations from "/Users/evands/adium/Utilities/dep-build-scripts/source/gst-plugins-base-0.10.23/gst-libs/gst/interfaces/tunerchannel.h" */
+GType gst_tuner_channel_flags_get_type (void);
+#define GST_TYPE_TUNER_CHANNEL_FLAGS (gst_tuner_channel_flags_get_type())
+G_END_DECLS
+
+#endif /* __GST_INTERFACES_ENUM_TYPES_H__ */
+
+/* Generated data ends here */
+
