7fa62a31a2f3c7f7c00dc4ec354ed8e0586f77ef
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2009-08-06 21:21:20 -0500

    Universalize the vv dependencies, universalize the gst plugins, and put the gst plugins into a PlugIns folder within libgstreamer.framework after universalizing them

--- Utilities/dep-build-scripts/universalize.sh
+++ Utilities/dep-build-scripts/universalize.sh
@@ -4,6 +4,9 @@ source common.sh
 setupDirStructure
 cd "$BUILDDIR"
 
+
+PURPLE_VERSION=0.6.0
+
 # create universal libraries for AdiumDeps.
 # "top-level" deps
 LIBINTL=libintl.8
@@ -19,9 +22,12 @@ SASL=libsasl2.2
 JSONGLIB=libjson-glib-1.0.0
 
 # vv
-# I'm not sure what to do to universalize the vv libs, especially the gst-plugins
+GSTREAMER=libgstreamer-0.10.0
+GSTBASE=libgstbase-0.10.0
+GSTFARSIGHT=libgstfarsight-0.10.0
+XML2=libxml2.2
+GSTINTERFACES=libgstinterfaces-0.10.0
 
-PURPLE_VERSION=0.6.0
 
 LIBPURPLE=libpurple.$PURPLE_VERSION
 PURPLE_FOLDER=libpurple-$PURPLE_VERSION
@@ -55,8 +61,39 @@ mkdir libgthread-2.0.0 || true
 # no headers to copy, make an empty file so that rtool isn't sad
 touch libgthread-2.0.0/no_headers_here.txt
 
+mkdir libmeanwhile-1 || true
+# no headers to copy, make an empty file so that rtool isn't sad
+touch libmeanwhile-1/no_headers_here.txt
+
+
 mkdir libjson-glib-1.0.0 || true
 cp -R $TARGET_DIR_I386/include/json-glib-1.0/ $UNIVERSAL_DIR/include/libjson-glib-1.0.0/
+
+
+####
+# VV includes
+###
+mkdir libgstreamer-0.10.0 || true
+cp -R $TARGET_DIR_I386/include/gstreamer-0.10 $UNIVERSAL_DIR/include/libgstreamer-0.10.0/
+
+for no_include_lib in $GSTBASE $GSTFARSIGHT $GSTINTERFACES; do
+    mkdir $no_include_lib || true
+    # no headers to copy, make an empty file so that rtool isn't sad
+    touch $no_include_lib/no_headers_here.txt
+done
+
+mkdir libxml-2.2 || true
+cp -R $TARGET_DIR_I386/include/libxml2 $UNIVERSAL_DIR/include/libxml-2.2/
+
+mkdir nice || true
+cp -R $TARGET_DIR_I386/include/nice $UNIVERSAL_DIR/include/nice/
+
+mkdir stun || true
+cp -R $TARGET_DIR_I386/include/stun $UNIVERSAL_DIR/include/stun/
+###
+# end VV includes
+####
+
 
 rm -rf $UNIVERSAL_DIR/include/$PURPLE_FOLDER
 cp -R $TARGET_DIR_I386/include/libpurple $UNIVERSAL_DIR/include/$PURPLE_FOLDER
@@ -66,10 +103,12 @@ cp $TARGET_DIR_PPC/include/libgadu.h $UNIVERSAL_DIR/include/$PURPLE_FOLDER/libga
 cp $SCRIPT_DIR/libgadu.h $UNIVERSAL_DIR/include/$PURPLE_FOLDER/
 cd ..
 
+
 cd $UNIVERSAL_DIR
 
-for lib in $LIBINTL $LIBGLIB $LIBGOBJECT $LIBGTHREAD $LIBGMODULE $MEANWHILE $JSONGLIB \
-           $GADU $LIBPURPLE; do
+for lib in $LIBINTL $LIBGLIB $LIBGOBJECT $LIBGTHREAD $LIBGMODULE $MEANWHILE \
+           $JSONGLIB $GADU $GSTREAMER $GSTBASE $GSTFARSIGHT $XML2 \
+           $GSTINTERFACES $LIBPURPLE; do
 	echo "Making $lib universal..."
 	python $SCRIPT_DIR/framework_maker/universalize.py \
 	  i386:$TARGET_DIR_I386/lib/$lib.dylib \
@@ -89,5 +128,23 @@ echo "Adding the Adium framework header."
 cp $SCRIPT_DIR/libpurple-full.h $PWD/Frameworks/libpurple.subproj/libpurple.framework/Headers/libpurple.h
 
 cp $SCRIPT_DIR/Libpurple-Info.plist $PWD/Frameworks/libpurple.subproj/libpurple.framework/Resources/Info.plist
+
+echo "Universalizing gstreamer plugins"
+
+# shell script goes here
+
+GST_PLUGINS="$BUILDDIR/Frameworks/libgstreamer.subproj/libgstreamer.framework/PlugIns"
+mkdir "$GST_PLUGINS" || true
+
+pushd "$TARGET_DIR_I386/lib/gstreamer-0.10/"
+for lib in *.so; do
+ if [ -f $TARGET_DIR_PPC/lib/gstreamer-0.10/$lib ]; then
+     echo "Universalizing gst plugin $lib"
+     lipo -create $TARGET_DIR_I386/lib/gstreamer-0.10/$lib $TARGET_DIR_PPC/lib/gstreamer-0.10/$lib -output "$GST_PLUGINS/$lib"
+ else
+    echo "$lib didn't build for PPC!"
+ fi
+done
+popd
 
 echo "Done - now run ./make_po_files.sh (if necessary) then ./copy_frameworks.sh"
