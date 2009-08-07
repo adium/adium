6ff5630f00c79e2c8c903b208fc3ceafd6edc7c3
Evan Schoenberg <Evan Schoenberg@localhost>
2009-08-06 21:25:11 -0500

    libgstreamer 0.10.24, gst-plugins-base 0.10.23, gst-plugins-good 0.10.15, and gst-plugins-bad 0.10.13

---
+++ Frameworks/libgstreamer.framework/Headers/gst/pbutils/pbutils-enumtypes.h
@@ -0,0 +1,19 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef __GST_INSTALL_ENUM_TYPES_H__
+#define __GST_INSTALL_ENUM_TYPES_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/* enumerations from "/Users/evands/adium/Utilities/dep-build-scripts/source/gst-plugins-base-0.10.23/gst-libs/gst/pbutils/install-plugins.h" */
+GType gst_install_plugins_return_get_type (void);
+#define GST_TYPE_INSTALL_PLUGINS_RETURN (gst_install_plugins_return_get_type())
+G_END_DECLS
+
+#endif /* __GST_INSTALL_ENUM_TYPES_H__ */
+
+/* Generated data ends here */
+
