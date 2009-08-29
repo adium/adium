2047c35f2047edf7bcd4655cc2a44f89300c8c55
Zachary West <zacwest@gmail.com>
2009-08-29 19:20:15 -0400

    Save and restore the "substitution" preferences on 10.5/10.6. For 10.5 users, things like quote replacement are now saved. For 10.6 users, the whole mess of them are.
    A few are now enabled by default, but just the things from before + the 10.6 "spelling" and "text replacement" substitutions.
    
    This also creates AILeopardCompatibility.h.

---
+++ Frameworks/AIUtilities Framework/Source/AILeopardCompatibility.h
@@ -0,0 +1,49 @@
+/*
+ *  AILeopardCompatibility.h
+ *  Adium
+ *
+ *  Created by Zachary West on 2009-08-29.
+ *  Copyright 2009  . All rights reserved.
+ *
+ */
+
+#ifndef AILeopardCompatibility
+#define AILeopardCompatibility
+
+#import <AvailabilityMacros.h>
+
+#ifndef MAC_OS_X_VERSION_10_6
+#define MAC_OS_X_VERSION_10_6 1060
+#endif //ndef MAC_OS_X_VERSION_10_6
+
+#if MAC_OS_X_VERSION_10_6 > MAC_OS_X_VERSION_MAX_ALLOWED
+
+#ifdef __OBJC__
+@interface NSTextView(NSTextViewLeopardMethods)
+- (void)setAutomaticDataDetectionEnabled:(BOOL)flag;
+- (BOOL)isAutomaticDataDetectionEnabled;
+- (void)toggleAutomaticDataDetection:(id)sender;
+
+- (void)setAutomaticDashSubstitutionEnabled:(BOOL)flag;
+- (BOOL)isAutomaticDashSubstitutionEnabled;
+- (void)toggleAutomaticDashSubstitution:(id)sender;
+
+- (void)setAutomaticTextReplacementEnabled:(BOOL)flag;
+- (BOOL)isAutomaticTextReplacementEnabled;
+- (void)toggleAutomaticTextReplacement:(id)sender;
+
+- (void)setAutomaticSpellingCorrectionEnabled:(BOOL)flag;
+- (BOOL)isAutomaticSpellingCorrectionEnabled;
+- (void)toggleAutomaticSpellingCorrection:(id)sender;
+@end
+#endif
+
+#else //Not compiling for 10.6
+
+#if !defined(NS_REQUIRES_NIL_TERMINATION)
+#define NS_REQUIRES_NIL_TERMINATION __attribute__((sentinel))
+#endif
+
+#endif //MAC_OS_X_VERSION_10_6
+
+#endif //AILeopardCompatibility
