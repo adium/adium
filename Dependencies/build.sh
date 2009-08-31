688513c581f3ffc7f3763155f00c2d0568dae7c3
Stephen Holt <sholt@steveholt.org>
2009-08-30 20:13:40 -0400

    Enable gstreamer and Break up its plugins into seperate processes.

--- Dependencies/build.sh
+++ Dependencies/build.sh
@@ -681,6 +681,9 @@ build_libpurple() {
 	quiet popd
 }
 
+##
+# xml2
+#
 build_libxml2() {
 	prereq "xml2" \
 		"ftp://xmlsoft.org:21//libxml2/libxml2-sources-2.7.3.tar.gz"
@@ -703,21 +706,14 @@ build_libxml2() {
 	quiet popd
 }
 
+
 ##
-# gstreamer plugins
+# liboil
 #
-build_gst_plugins() {
+build_liboil() {
 	prereq "oil" \
 		"http://liboil.freedesktop.org/download/liboil-0.3.16.tar.gz"
-	prereq "gst-plugins-base" \
-		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-0.10.23.tar.gz"
-	prereq "gst-plugins-good" \
-		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-good-0.10.15.tar.gz"
-	prereq "gst-plugins-bad" \
-		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-bad-0.10.13.tar.gz"
-	prereq "gst-plugins-farsight" \
-		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-farsight-0.12.11.tar.gz"
-
+	
 	quiet pushd "$ROOTDIR/source/oil"
 	
 	if needsconfigure $@; then
@@ -728,14 +724,120 @@ build_gst_plugins() {
 				--disable-dependency-tracking
 	fi
 	
-	status "Building and installing gstreamer"
-	warning "Building too much! Patch the Makefile"
+	status "Building and installing oil"
 	make -j $NUMBER_OF_CORES
 	make install
 	
 	quiet popd
 }
 
+##
+# gst-plugins-base
+#
+build_gst_plugins_base() {
+	prereq "gst-plugins-base" \
+		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-0.10.23.tar.gz"
+	
+	quiet pushd "$ROOTDIR/source/gst-plugins-base"
+	
+	if needsconfigure $@; then
+		status "Configuring gst-plugins-base"
+		CFLAGS="$ARCH_CFLAGS" LDFLAGS="$ARCH_LDFLAGS" \
+			./configure \
+				--prefix="$ROOTDIR/build" \
+				--disable-dependency-tracking
+	fi
+	
+	status "Building and installing gst-plugins-base"
+	make -j $NUMBER_OF_CORES
+	make install
+	
+	quiet popd
+}
+
+##
+# gst-plugins-good
+#
+build_gst_plugins_good() {
+	prereq "gst-plugins-good" \
+		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-good-0.10.15.tar.gz"
+	
+	quiet pushd "$ROOTDIR/source/gst-plugins-good"
+	
+	if needsconfigure $@; then
+		status "Configuring gst-plugins-good"
+		CFLAGS="$ARCH_CFLAGS" LDFLAGS="$ARCH_LDFLAGS" \
+			./configure \
+				--prefix="$ROOTDIR/build" \
+				--disable-aalib \
+				--disable-dependency-tracking
+	fi
+	
+	status "Building and installing gst-plugins-good"
+	make -j $NUMBER_OF_CORES
+	make install
+	
+	quiet popd
+}
+
+##
+# gst-plugins-bad
+#
+build_gst_plugins_bad() {
+	prereq "gst-plugins-bad" \
+		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-bad-0.10.13.tar.gz"
+	
+	quiet pushd "$ROOTDIR/source/gst-plugins-bad"
+	
+	if needsconfigure $@; then
+		status "Configuring gst-plugins-bad"
+		CFLAGS="$ARCH_CFLAGS" LDFLAGS="$ARCH_LDFLAGS" \
+			./configure \
+				--prefix="$ROOTDIR/build" \
+				--disable-dependency-tracking
+	fi
+	
+	status "Building and installing gst-plugins-bad"
+	make -j $NUMBER_OF_CORES
+	make install
+	
+	quiet popd
+}
+
+##
+# gst-plugins-farsight
+#
+build_gst_plugins_farsight() {
+	prereq "gst-plugins-farsight" \
+		"http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-farsight-0.12.11.tar.gz"
+	
+	quiet pushd "$ROOTDIR/source/gst-plugins-farsight"
+	
+	if needsconfigure $@; then
+		status "Configuring gst-plugins-farsight"
+		CFLAGS="$ARCH_CFLAGS" LDFLAGS="$ARCH_LDFLAGS" \
+			./configure \
+				--prefix="$ROOTDIR/build" \
+				--disable-dependency-tracking
+	fi
+	
+	status "Building and installing gst-plugins-farsight"
+	make -j $NUMBER_OF_CORES
+	make install
+	
+	quiet popd
+}
+
+##
+# gstreamer plugins
+#
+build_gst_plugins() {
+#	build_liboil $@
+#	build_gst_plugins_base $@
+#	build_gst_plugins_good $@
+#	build_gst_plugins_bad $@
+#	build_gst_plugins_farsight $@
+}
 
 ##
 # gstreamer
@@ -750,10 +852,12 @@ build_gstreamer() {
 	
 	if needsconfigure $@; then
 		status "Configuring gstreamer"
-		CFLAGS="$ARCH_CFLAGS" LDFLAGS="$ARCH_LDFLAGS" \
-			./configure \
-				--prefix="$ROOTDIR/build" \
-				--disable-dependency-tracking
+		CONFIG_CMD="./configure \
+				--prefix=$ROOTDIR/build \
+				--disable-dependency-tracking"
+		xconfigure "${BASE_CFLAGS}" "${BASE_LDFLAGS}" "${CONFIG_CMD}" \
+			"$ROOTDIR/source/gstreamer/gst/gstconfig.h" \
+			"$ROOTDIR/source/gstreamer/config.h"
 	fi
 	
 	status "Building and installing gstreamer"
@@ -763,9 +867,8 @@ build_gstreamer() {
 	
 	quiet popd
 	
-	build_gst_plugins
+	build_gst_plugins $@
 }
-
 
 ##
 # make_po_files
@@ -836,9 +939,10 @@ build_gadugadu $@
 
 build_intltool $@
 build_jsonglib $@
-build_libpurple $@
 
-#build_gstreamer $@
+build_gstreamer $@
+
+#build_libpurple $@
 
 #build_sipe $@
 #build_gfire $@
