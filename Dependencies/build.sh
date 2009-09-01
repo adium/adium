c91f510ce7c467c0beaa65fd6fc2da69104572df
Stephen Holt <sholt@steveholt.org>
2009-09-01 16:05:52 -0400

    Frameworkize!  Use a symlink to the python script and rtool in Utilities/dep-build-scripts for now.

--- Dependencies/build.sh
+++ Dependencies/build.sh
@@ -1016,10 +1016,127 @@ make_po_files() {
 }
 
 ##
+# prep_headers
+#
+prep_headers() {
+	GLIB_VERSION=2.0
+	GSTREAMER_VERSION=0.10
+	INTL_VERSION=8
+	JSON_GLIB_VERSION=1.0
+	MEANWHILE_VERSION=1
+	XML_VERSION=2.2
+	LIBPURPLE_VERSION=0.6.0
+	
+	## purple prereqs
+	quiet mkdir "${ROOTDIR}/build/lib/include" || true
+	#libintl
+	status "Staging libintl headers"
+	local libintlDir="${ROOTDIR}/build/lib/include/libintl-${INTL_VERSION}"
+	quiet mkdir "${libintlDir}" || true
+	cp "${ROOTDIR}/build/include/libintl.h" "${libintlDir}/"
+	
+	#glib
+	status "Staging glib headers"
+	local glibDir="${ROOTDIR}/build/lib/include/libglib-${GLIB_VERSION}.0"
+	cp -R "${ROOTDIR}/build/include/glib-${GLIB_VERSION}" "${glibDir}/"
+	
+	#gmodule
+	status "Staging gmodule headers"
+	local gmoduleDir="${ROOTDIR}/build/lib/include/libgmodule-${GLIB_VERSION}.0"
+	quiet mkdir "${gmoduleDir}" || true
+	cp "${ROOTDIR}/build/include/glib-${GLIB_VERSION}/gmodule.h" "${gmoduleDir}"
+	
+	#gobject
+	status "Staging gobject headers"
+	local gobjectDir="${ROOTDIR}/build/lib/include/libgobject-${GLIB_VERSION}.0"
+	quiet mkdir "${gobjectDir}" || true
+	cp "${ROOTDIR}/build/include/glib-${GLIB_VERSION}/glib-object.h" "${gobjectDir}"
+	cp -R "${ROOTDIR}/build/include/glib-${GLIB_VERSION}/gobject/" "${gobjectDir}"
+	
+	#gthread
+	status "Staging gthread non-headers"
+	local gthreadDir="${ROOTDIR}/build/lib/include/libgthread-${GLIB_VERSION}.0"
+	quiet mkdir "${gthreadDir}" || true
+	touch "${gthreadDir}/no_headers_here.txt"
+	
+	#meanwhile
+	status "Staging meanwhile non-headers"
+	local meanwhileDir="${ROOTDIR}/build/lib/include/libmeanwhile-${MEANWHILE_VERSION}"
+	quiet mkdir "${meanwhileDir}" || true
+	touch "${meanwhileDir}/no_headers_here.txt"
+	
+	#json-glib
+	status "Staging json-glib headers"
+	local jsonDir="${ROOTDIR}/build/lib/include/libjson-glib-${JSON_GLIB_VERSION}.0"
+	quiet mkdir "${jsonDir}" || true
+	cp -R "${ROOTDIR}/build/include/json-glib-${JSON_GLIB_VERSION}/json-glib" "${jsonDir}"
+	
+	## VV stuff
+	
+	#gstreamer
+	status "Staging gstreamer and plugins headers"
+	local gstDir="${ROOTDIR}/build/lib/include/libgstreamer-${GSTREAMER_VERSION}.0"
+	cp -R "${ROOTDIR}/build/include/gstreamer-${GSTREAMER_VERSION}" "${gstDir}"
+	
+	local non_includes=( "libgstbase-${GSTREAMER_VERSION}.0" \
+						 "libgstfarsight-${GSTREAMER_VERSION}.0" \
+						 "libgstinterfaces-${GSTREAMER_VERSION}.0" )
+	for no_include_lib in ${non_includes[@]} ; do
+		quiet mkdir "${ROOTDIR}/build/lib/include/${no_include_lib}" || true
+		touch "${ROOTDIR}/build/lib/include/${no_include_lib}/no_headers_here.txt"
+	done
+	
+	#libxml
+	status "Staging libxml headers"
+	local xml2Dir="${ROOTDIR}/build/lib/include/libxml-${XML_VERSION}"
+	quiet mkdir "${xml2Dir}" || true
+	cp -R "${ROOTDIR}/build/include/libxml2" "${xml2Dir}"
+	
+	#libpurple
+	status "Staging libpurple headers"
+	local purpleDir="${ROOTDIR}/build/lib/include/libpurple-${LIBPURPLE_VERSION}"
+	quiet rm -rf "${purpleDir}"
+	cp -R "${ROOTDIR}/build/include/libpurple" "${purpleDir}"
+	cp "${ROOTDIR}/build/include/libgadu.h" "${purpleDir}/"
+	status "Completed staging headers"
+}
+
+##
 # make_framework
 #
 make_framework() {
-	warning "Not yet implemented."
+	FRAMEWORK_DIR="${ROOTDIR}/Frameworks"
+	quiet mkdir "${FRAMEWORK_DIR}"
+	
+	prep_headers
+	
+	export PATH="${ROOTDIR}/rtool:$PATH"
+	
+	# resolve symlinks - rtool doesn't like lthem :(
+	status "Resolving symlinks for frameworkize.py..."
+	local files="${ROOTDIR}/build/lib/*.dylib"
+	for file in ${files} ; do
+		if [ -h ${file} ] ; then
+			local resolvedLink=`/usr/bin/readlink -n ${file}`
+			status "... ${file} -> ${ROOTDIR}/build/lib/${resolvedLink}"
+			rm "${file}"
+			cp "${ROOTDIR}/build/lib/${resolvedLink}" "${file}"
+		fi
+	done
+	
+	status "Making a framework for libpurple-${LIBPURPLE_VERSION} and all dependencies..."
+	python "${ROOTDIR}/framework_maker/frameworkize.py" \
+		"${ROOTDIR}/build/lib/libpurple.${LIBPURPLE_VERSION}.dylib" \
+		"${FRAMEWORK_DIR}"
+	
+	status "Adding the Adium framework header..."
+	cp "${ROOTDIR}/libpurple-full.h" \
+		"${FRAMEWORK_DIR}/libpurple.subproj/libpurple.framework/Headers/libpurple.h"
+
+	cp "${ROOTDIR}/Libpurple-Info.plist" \
+		"${FRAMEWORK_DIR}/libpurple.subproj/libpurple.framework/Resources/Info.plist"
+	
+	status "Done."
 }
 
 # Check that we're in the Dependencies directory
@@ -1083,9 +1200,9 @@ build_gstreamer $@
 build_farsight $@
 
 build_libpurple $@
+make_framework $@
 
 #build_sipe $@
 #build_gfire $@
 
 #make_po_files $@
-#make_framework $@
