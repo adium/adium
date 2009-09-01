c33f97a8b4e0322cb04cd9f55a76d7701837555b
Stephen Holt <sholt@steveholt.org>
2009-09-01 16:20:12 -0400

    Copies and slightly modified version of copy_frameworks.sh.  Not in the main build process so there's no accidental clobbering of frameworks

---
+++ Dependencies/copy_frameworks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eu
+
+ROOTDIR=$(pwd)
+if ! expr "$ROOTDIR" : '.*/Dependencies$' &> /dev/null; then
+	error "Please run this script from the Dependencies directory."
+	exit 1
+fi
+
+ADIUM="`dirname $0`/.."
+
+cp -r "$ROOTDIR"/Frameworks/*.subproj/*.framework "$ADIUM/Frameworks/"
+
+pushd "$ADIUM/build" > /dev/null 2>&1
+	rm -rf */AdiumLibpurple.framework 
+	rm -rf */*/Adium.app/Contents/Frameworks/lib*
+popd > /dev/null 2>&1
+
+echo "Done - now build Adium"
