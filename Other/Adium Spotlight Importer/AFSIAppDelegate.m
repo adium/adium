abf32b984d99efe198068be3a694e79a1eb5af6b
Peter Hosey <git@boredzo.org>
2009-09-12 11:39:55 -0700

    Added code to the test app to track how many files it's ?imported? and how long it's taken. This reports 0.007231 seconds per file on my machine.

--- Other/Adium Spotlight Importer/AFSIAppDelegate.m
+++ Other/Adium Spotlight Importer/AFSIAppDelegate.m
@@ -15,12 +15,13 @@ extern Boolean GetMetadataForFile(void* thisInterface,
 
 @implementation AFSIAppDelegate
 
-- (void) importOneChatlogFromPath:(NSString *)path {
+- (void) importOneChatlogFromPath:(NSString *)path numFilesProcessed:(inout NSUInteger *)numFilesProcessed {
 	NSError *error = nil;
 	GetMetadataForFile(NULL, [NSMutableDictionary dictionary], [[NSWorkspace sharedWorkspace] typeOfFile:path error:&error], path);
+	++*numFilesProcessed;
 }
 
-- (void) importChatlogsFromDirectoryTreeRootedAtPath:(NSString *)rootPath {
+- (void) importChatlogsFromDirectoryTreeRootedAtPath:(NSString *)rootPath numFilesProcessed:(inout NSUInteger *)numFilesProcessedPtr {
 	NSFileManager *mgr = [NSFileManager defaultManager];
 	NSDirectoryEnumerator *dirEnum = [mgr enumeratorAtPath:rootPath];
 	for (NSString *subpath in dirEnum) {
@@ -30,28 +31,35 @@ extern Boolean GetMetadataForFile(void* thisInterface,
 			BOOL isDir = NO;
 			if ([mgr fileExistsAtPath:path isDirectory:&isDir] && isDir)
 				[dirEnum skipDescendents];
-			[self importOneChatlogFromPath:path];
+			[self importOneChatlogFromPath:path numFilesProcessed:numFilesProcessedPtr];
 		} else if ([ext caseInsensitiveCompare:@"AdiumLog"] == NSOrderedSame)
-			[self importOneChatlogFromPath:path];
+			[self importOneChatlogFromPath:path numFilesProcessed:numFilesProcessedPtr];
 		else if ([ext caseInsensitiveCompare:@"AdiumHTMLLog"] == NSOrderedSame)
-			[self importOneChatlogFromPath:path];
+			[self importOneChatlogFromPath:path numFilesProcessed:numFilesProcessedPtr];
 	}
 }
 
 - (void) application:(NSApplication *)sender openFiles:(NSArray *)paths {
+	NSUInteger numFilesProcessed = 0;
+	NSDate *start, *end;
+	start = [NSDate date];
+
 	NSFileManager *mgr = [NSFileManager defaultManager];
 	for (NSString *path in paths) {
 		BOOL isDir = NO;
 		if ([mgr fileExistsAtPath:path isDirectory:&isDir]) {
 			if (isDir)
-				[self importChatlogsFromDirectoryTreeRootedAtPath:path];
+				[self importChatlogsFromDirectoryTreeRootedAtPath:path numFilesProcessed:&numFilesProcessed];
 			else
-				[self importOneChatlogFromPath:path];
+				[self importOneChatlogFromPath:path numFilesProcessed:&numFilesProcessed];
 		}
 	}
 
+	end = [NSDate date];
+	NSTimeInterval timeTaken = [end timeIntervalSinceDate:start];
+
 	NSBeep();
-	NSLog(@"Job's done!");
+	NSLog(@"Job's done! Processed %lu files in %f seconds (%f seconds per file)", numFilesProcessed, timeTaken, timeTaken / numFilesProcessed);
 }
 
 @end
