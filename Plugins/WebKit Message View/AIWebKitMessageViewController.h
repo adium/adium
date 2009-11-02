e697e9b54e33efdcf6fd09a1055b25cd0c3458bc
Zachary West <zacwest@gmail.com>
2009-11-02 18:30:22 -0500

    Instead of inserting a <hr/> when we lose focus, which ends up breaking more than you'd expect, add a message class for the next message. Fixes #13300.
    This removes the "Show Focus Lines" preference (always a good thing), and always inserts the mark in the scrollbar. It will be up to the style to implement the "focus" class to show the location. All previous messages of class "focus" will have "focus" removed when focus is lost again.

--- Plugins/WebKit Message View/AIWebKitMessageViewController.h
+++ Plugins/WebKit Message View/AIWebKitMessageViewController.h
@@ -47,6 +47,9 @@
 	NSImage						*imageMask;
 	NSMutableArray				*objectsWithUserIconsArray;
 	NSMutableDictionary			*objectIconPathDict;
+	
+	//Focus tracking
+	BOOL						nextMessageFocus;
 }
 
 /*!
