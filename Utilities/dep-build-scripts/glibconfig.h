1e1eee72aebe27d34790eba828cfedce687758d0
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2009-11-04 18:29:43 -0600

    Removed obsolete dep-build-scripts to prevent potential confusion, since the modern build scripts work so well

--- Utilities/dep-build-scripts/glibconfig.h
+++
@@ -1,7 +0,0 @@
-#ifdef __ppc__
-#include <glibconfig-ppc.h>
-#elif defined(__i386__)
-#include <glibconfig-i386.h>
-#else
-#error This isn't a recognized platform.
-#endif
--- Utilities/dep-build-scripts/rtool/Resources/Examples/rpath/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, const char * argv[])
-{
-	printf("-- main() returns libfoo:foo() --\n");
-	
-	getNSGetExecutablePath((char *)argv[0]);
-	return foo();
-}
