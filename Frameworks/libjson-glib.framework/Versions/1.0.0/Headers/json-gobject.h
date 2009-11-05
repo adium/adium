976a47cae3af094d09db3c28593a29d0d2f924b9
Zachary West <zacwest@gmail.com>
2009-11-04 23:41:42 -0500

    Updated libjson-glib, moreso, this time. I think.

--- Frameworks/libjson-glib.framework/Versions/1.0.0/Headers/json-gobject.h
+++ Frameworks/libjson-glib.framework/Versions/1.0.0/Headers/json-gobject.h
@@ -2,19 +2,23 @@
  * 
  * This file is part of JSON-GLib
  * Copyright (C) 2007  OpenedHand Ltd.
+ * Copyright (C) 2009  Intel Corp.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
  * Author:
- *   Emmanuele Bassi  <ebassi@openedhand.com>
+ *   Emmanuele Bassi  <ebassi@linux.intel.com>
  */
 
 #ifndef __JSON_GOBJECT_H__
