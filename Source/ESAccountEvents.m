dcf6fdd1a2c5d287ecfca3f4ccab38b9ebdce5c0
Stephen Holt <sholt@steveholt.org>
2010-02-03 16:37:07 -0500

    Initialize to nil here and silence a warning.

--- Source/ESAccountEvents.m
+++ Source/ESAccountEvents.m
@@ -187,7 +187,7 @@
 									forChat:(AIChat *)chat
 								  withCount:(NSUInteger)count
 {
-	NSString *format;
+	NSString *format = nil;
 	
 	if ([eventID isEqualToString:ACCOUNT_CONNECTED]) {
 		format = AILocalizedString(@"%u accounts connected",nil);
