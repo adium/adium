eab1b0453be9eebe9b7b254b6b53284088b45de6
Evan Schoenberg <Evan Schoenberg@localhost>
2010-05-20 12:01:18 -0500

    copy_frameworks.sh now gives better feedback and runs to completion

--- Dependencies/copy_frameworks.sh
+++ Dependencies/copy_frameworks.sh
@@ -8,13 +8,18 @@ fi
 
 ADIUM="`dirname $0`/.."
 
-cp -R "$ROOTDIR"/Frameworks/*.subproj/*.framework "$ADIUM/Frameworks/"
+# Don't allow any out-of-date plugins to hang around and be subsequently laoded
+rm -rf "$ADIUM/Frameworks/libgstreamer.framework/PlugIns"
+
+cp -Rf "$ROOTDIR"/Frameworks/*.subproj/*.framework "$ADIUM/Frameworks/" || TRUE
 
 # These libgst plugins cause problems in gst_init if present; we shouldn't
 # be building them at all.
 rm "$ADIUM/Frameworks/libgstreamer.framework/PlugIns/libgstwavenc.so"
 rm "$ADIUM/Frameworks/libgstreamer.framework/PlugIns/libgstwavparse.so"
 rm "$ADIUM/Frameworks/libgstreamer.framework/PlugIns/libgsty4menc.so"
+
+echo "Cleaning the Adium built products"
 
 pushd "$ADIUM/build" > /dev/null 2>&1
 	rm -rf */AdiumLibpurple.framework 
