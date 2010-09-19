895f0dd2a4b735ac742e064116726696480cca31
Evan Schoenberg <Evan Schoenberg@localhost>
2010-09-19 15:54:28 -0500

    Useful debug logging (transplanted from 9aed283a5ddd0555e87bc9e64eb998d7b06aeea9)

--- Source/AIListController.m
+++ Source/AIListController.m
@@ -699,9 +699,12 @@
 		
 		[[AIContactObserverManager sharedManager] delayListObjectNotifications];
 
+		AILogWithSignature(@"Dropping into %@ (%@)", item, item.listObject);
+
 		//Move the list object to its new location
 		if ([item.listObject isKindOfClass:[AIListGroup class]]) {
 			/* Can't drop into the offline group */
+
 			if (item.listObject != adium.contactController.offlineGroup) {
 				AIListGroup *group = (AIListGroup *)(item.listObject);
 				
@@ -733,6 +736,8 @@
 
 							// Contact being moved to a new group.
 							// Holding option copies into the new group (like in Finder)
+							
+							AILogWithSignature(@"Moving %@ from %@ to %@", listObject, sourceGroups, group);
 							[adium.contactController moveContact:(AIListContact *)listObject
 													  fromGroups:sourceGroups
 													  intoGroups:[NSSet setWithObject:group]];
