ecff613aceba86860137e4edfee6d4d6edb7bcd2
Evan Schoenberg <Evan Schoenberg@localhost>
2010-11-15 18:39:03 -0600

    Disable connecting via SSL to ICQ (transplanted from ce0971e07ac63a6c59b58ccc03ea5708a192d7e0)

--- Plugins/Purple Service/ESPurpleICQAccountViewController.m
+++ Plugins/Purple Service/ESPurpleICQAccountViewController.m
@@ -32,6 +32,7 @@
 	[popUp_encoding setMenu:[self encodingMenu]];
 }
 
+
 //Configure controls
 - (void)configureForAccount:(AIAccount *)inAccount
 {
@@ -40,6 +41,10 @@
 																		group:GROUP_ACCOUNT_STATUS]];
 	[checkBox_webAware setState:[[account preferenceForKey:KEY_ICQ_WEB_AWARE
 													 group:GROUP_ACCOUNT_STATUS] boolValue]];
+
+#warning Remove when ICQ SSL support is fixed
+	[checkBox_SSL setState:NO];
+	[checkBox_SSL setEnabled:NO];
 }
 
 //Save controls
