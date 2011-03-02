1e3aee9fd4d54f23c036cb603961d383baedcdbf
Stephen Holt <sholt@adium.im>
2011-03-01 20:45:11 -0800

    fixes for various type warnings, including unimplimented protocols.

--- Frameworks/Adium Framework/Source/AIContactMenu.h
+++ Frameworks/Adium Framework/Source/AIContactMenu.h
@@ -14,6 +14,7 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#import <AIUtilities/AILeopardCompatibility.h>
 #import <Adium/AIAbstractListObjectMenu.h>
 #import <Adium/AIContactObserverManager.h>
 
@@ -21,7 +22,7 @@
 
 @protocol AIContactMenuDelegate;
 
-@interface AIContactMenu : AIAbstractListObjectMenu <AIListObjectObserver> {
+@interface AIContactMenu : AIAbstractListObjectMenu <AIListObjectObserver,NSMenuDelegate> {
 	AIListObject			*containingObject;
 	
 	id<AIContactMenuDelegate>						delegate;
--- Source/AdiumSetupWizard.h
+++ Source/AdiumSetupWizard.h
@@ -14,11 +14,12 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#import	<AIUtilities/AILeopardCompatibility.h>
 #import <Adium/AIWindowController.h>
 
 @class SetupWizardBackgroundView;
 
-@interface AdiumSetupWizard : AIWindowController {
+@interface AdiumSetupWizard : AIWindowController <NSTabViewDelegate> {
 	IBOutlet SetupWizardBackgroundView	*backgroundView;
 
 	IBOutlet NSButton	*button_continue;
