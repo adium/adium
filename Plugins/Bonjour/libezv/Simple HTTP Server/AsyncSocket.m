6e460b0722d4b95ca2bfecd7934f480fe1bf9895
Stephen Holt <sholt@steveholt.org>
2011-03-15 23:42:57 -0700

    But we do need a formatted string here...

--- Plugins/Bonjour/libezv/Simple HTTP Server/AsyncSocket.m
+++ Plugins/Bonjour/libezv/Simple HTTP Server/AsyncSocket.m
@@ -552,13 +552,13 @@ Failed:;
 	if(theDelegate == NULL)
 	{
 		NSString *message = @"Attempting to connect without a delegate. Set a delegate first.";
-		[NSException raise:AsyncSocketException format:message];
+		[NSException raise:AsyncSocketException format:@"%@",message];
 	}
 
 	if(theSocket != NULL || theSocket6 != NULL)
 	{
 		NSString *message = @"Attempting to connect while connected or accepting connections. Disconnect first.";
-		[NSException raise:AsyncSocketException format:message];
+		[NSException raise:AsyncSocketException format:@"%@",message];
 	}
 	
 	BOOL pass = YES;
@@ -595,13 +595,13 @@ Failed:;
 	if (theDelegate == NULL)
 	{
 		NSString *message = @"Attempting to connect without a delegate. Set a delegate first.";
-		[NSException raise:AsyncSocketException format:message];
+		[NSException raise:AsyncSocketException format:@"%@",message];
 	}
 	
 	if (theSocket != NULL || theSocket6 != NULL)
 	{
 		NSString *message = @"Attempting to connect while connected or accepting connections. Disconnect first.";
-		[NSException raise:AsyncSocketException format:message];
+		[NSException raise:AsyncSocketException format:@"%@",message];
 	}
 	
 	BOOL pass = YES;
