405da791835147b53b09cbe2e9b64a555a42b307
Evan Schoenberg <Evan Schoenberg@localhost>
2011-06-24 14:20:49 -0500

    Update to our new app id, api key, and app secret. Also, prevent a crash by not returning nil within the webView willLoadResource method. (transplanted from da2a143f916836c30d1322dc2516ff2729147eae)

--- Plugins/Purple Service/AIFacebookXMPPAccount.m
+++ Plugins/Purple Service/AIFacebookXMPPAccount.m
@@ -87,8 +87,10 @@ enum {
 
 	purple_account_set_username(account, self.purpleAccountName);
 	purple_account_set_string(account, "connection_security", "");
-    purple_account_set_string(account, "fb_api_key", "819fd0b010d15eac1b36648f0126ece3");
-    purple_account_set_string(account, "fb_api_secret", [[self preferenceForKey:@"FBSessionSecret" group:GROUP_ACCOUNT_STATUS] UTF8String]);
+    purple_account_set_string(account, "fb_api_key", API_KEY);
+    purple_account_set_string(account, "fb_api_secret", APP_SECRET
+							  
+							  /*[[self preferenceForKey:@"FBSessionSecret" group:GROUP_ACCOUNT_STATUS] UTF8String]*/);
 }
 
 - (const char *)purpleAccountName
