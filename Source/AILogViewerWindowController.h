7afd4b3a9da5ef7e875cd4f99b13463824dbcf29
Patrick Steinhardt <ps@pks.im>
2011-07-26 18:26:23 +0200

    Small code cleanups to AILogViewerWindowController.

--- Source/AILogViewerWindowController.h
+++ Source/AILogViewerWindowController.h
@@ -15,35 +15,6 @@
  */
 
 #import <Adium/AIWindowController.h>
-#import <AIUtilities/AIDividedAlternatingRowOutlineView.h>
-
-@class AIChatLog, AILoggerPlugin;
-
-#define LOG_SEARCH_STATUS_INTERVAL		20	//1/60ths of a second to wait before refreshing search status
-
-#define LOG_CONTENT_SEARCH_MAX_RESULTS	10000	//Max results allowed from a search
-#define LOG_RESULT_CLUMP_SIZE			10	//Number of logs to fetch at a time
-
-#define SEARCH_MENU						AILocalizedString(@"Search Menu",nil)
-#define FROM							AILocalizedString(@"From",nil)
-#define TO								AILocalizedString(@"To",nil)
-
-#define ACCOUNT							AILocalizedString(@"Account",nil)
-#define DESTINATION						AILocalizedString(@"Destination",nil)
-
-#define DATE							AILocalizedString(@"Date",nil)
-#define CONTENT							AILocalizedString(@"Content",nil)
-#define DELETE							AILocalizedString(@"Delete",nil)
-#define DELETEALL						AILocalizedString(@"Delete All",nil)
-#define SEARCH							AILocalizedString(@"Search",nil)
-
-#define	KEY_LOG_VIEWER_EMOTICONS			@"Log Viewer Emoticons"
-#define	KEY_LOG_VIEWER_TIMESTAMPS			@"Log Viewer Timestamps"
-#define KEY_LOG_VIEWER_SELECTED_COLUMN		@"Log Viewer Selected Column Identifier"
-#define	LOG_VIEWER_DID_CREATE_LOG_ARRAYS	@"LogViewerDidCreateLogArrays"
-#define	LOG_VIEWER_DID_UPDATE_LOG_ARRAYS	@"LogViewerDidUpdateLogArrays"
-
-#define DATE_ITEM_IDENTIFIER			@"date"
 
 typedef enum {
     LOG_SEARCH_FROM = 0,
@@ -65,34 +36,33 @@ typedef enum {
 	AIDateTypeAfter
 } AIDateType;
 
-@class AIListContact, AIChat, AIAccount, AIGradientView;
+@class AIAccount, AIChatLog, AIDividedAlternatingRowOutlineView, AIGradientView, AIListContact, AILoggerPlugin;
 
 @interface AILogViewerWindowController : AIWindowController <NSToolbarDelegate, NSOutlineViewDelegate, NSTableViewDelegate> {
-	AILoggerPlugin				*plugin;
+	AILoggerPlugin					*plugin;
 
 	IBOutlet	AIDividedAlternatingRowOutlineView	*outlineView_contacts;
 
-	IBOutlet	NSSplitView		*splitView_contacts;
-	IBOutlet	NSSplitView		*splitView_logs;
-	IBOutlet	NSTableView		*tableView_results;
-	IBOutlet	NSTextView		*textView_content;
+	IBOutlet	NSSplitView			*splitView_contacts;
+	IBOutlet	NSSplitView			*splitView_logs;
+	IBOutlet	NSTableView			*tableView_results;
+	IBOutlet	NSTextView			*textView_content;
 
-	IBOutlet    NSView			*view_SearchField;
-	IBOutlet    NSButton		*button_deleteLogs;
+	IBOutlet    NSView				*view_SearchField;
 
-	IBOutlet	NSView			*view_DatePicker;
-	IBOutlet	NSPopUpButton	*popUp_dateFilter;
+	IBOutlet	NSView				*view_DatePicker;
+	IBOutlet	NSPopUpButton		*popUp_dateFilter;
 
 	IBOutlet	NSTextField			*textField_resultCount;
 	IBOutlet	NSProgressIndicator	*progressIndicator;
 	IBOutlet	NSTextField			*textField_progress;
 
-	IBOutlet	NSSearchField	*searchField_logs;
+	IBOutlet	NSSearchField		*searchField_logs;
 
-	IBOutlet	NSDatePicker	*datePicker;
+	IBOutlet	NSDatePicker		*datePicker;
 
-	IBOutlet	AIGradientView	*view_FindNavigator;
-	IBOutlet	NSTextField		*textField_findCount;
+	IBOutlet	AIGradientView		*view_FindNavigator;
+	IBOutlet	NSTextField			*textField_findCount;
 	IBOutlet	NSSegmentedControl	*segment_previousNext;
 
 	//Array of selected / displayed logs.  (Locked access)
@@ -105,27 +75,25 @@ typedef enum {
 	NSTableColumn		*selectedColumn;		//Selected/active sort column
 
 	//Search information
-	NSInteger					activeSearchID;			//ID of the active search thread, all other threads should quit
-	NSLock				*searchingLock;			//Locked when a search is in progress
+	NSInteger			activeSearchID;			//ID of the active search thread, all other threads should quit
 	BOOL				searching;				//YES if a search is in progress
 	NSString			*activeSearchString;	//Current search string
 	BOOL				suppressSearchRequests;
 	BOOL				isOpeningForContact;
-	NSInteger					indexingUpdatesReceivedWhileSearching; //Number of times indexing has updated during the current search
+	NSInteger			indexingUpdatesReceivedWhileSearching; //Number of times indexing has updated during the current search
 	NSMutableArray		*matches;
 	NSInteger			currentMatch;
 
 	BOOL				sortDirection;			//Direction to sort
 
 	NSTimer				*refreshResultsTimer;
-	NSInteger					searchIDToReattemptWhenComplete;
+	NSInteger			searchIDToReattemptWhenComplete;
 
-	NSString			*filterForAccountName;	//Account name to restrictively match content searches
 	NSMutableSet		*contactIDsToFilter;
 
 	AIDateType			filterDateType;
 	NSCalendarDate		*filterDate;
-	NSInteger					firstDayOfWeek;
+	NSInteger			firstDayOfWeek;
 	BOOL				iCalFirstDayOfWeekDetermined;
 
 	NSMutableDictionary	*logToGroupDict;
@@ -140,16 +108,10 @@ typedef enum {
 	NSImage				*adiumIcon;
 	NSImage				*adiumIconHighlighted;
 
-	NSMutableArray		*fromArray;				//Array of account names
-	NSMutableArray		*fromServiceArray;		//Array of services for accounts
 	NSMutableArray		*toArray;				//Array of contacts
-	NSMutableArray		*toServiceArray;		//Array of services for accounts
 	NSDateFormatter		*headerDateFormatter;	//Format for dates displayed in the content text view
 
-	NSInteger					sameSelection;
-	BOOL				useSame;
-
-	NSInteger					cachedSelectionIndex;
+	NSInteger			cachedSelectionIndex;
 	BOOL				deleteOccurred;			// YES only if a delete occurs, allowing the table to preserve selection after a search begins
 
 	NSString			*horizontalRule;
@@ -158,11 +120,11 @@ typedef enum {
 
 	NSNumber			*allContactsIdentifier;
 	//Old
-	BOOL showEmoticons;
-	BOOL showTimestamps;
+	BOOL				showEmoticons;
+	BOOL				showTimestamps;
 
-	SKSearchRef currentSearch;
-	NSLock		*currentSearchLock;
+	SKSearchRef			currentSearch;
+	NSLock				*currentSearchLock;
 	
 	NSInvocationOperation *displayOperation;
 }
@@ -177,7 +139,6 @@ typedef enum {
 - (void)stopSearching;
 
 - (void)displayLog:(AIChatLog *)log;
-- (void)installToolbar;
 
 - (void)setSearchMode:(LogSearchMode)inMode;
 - (void)setSearchString:(NSString *)inString;
