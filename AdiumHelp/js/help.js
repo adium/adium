570165f9a7b2664f7652fc35779bcf43225b9fb0
Adrian Godoroja <robotive@icloud.com>
2011-08-04 22:53:59 +0300

    Updated documentation to show navbar on systems != Lion.

--- AdiumHelp/js/help.js
+++ AdiumHelp/js/help.js
@@ -5,4 +5,7 @@ if (parseInt(/Mac OS X 10_([^\s]+)/.exec(navigator.appVersion)[1]) < 7) {
 	var nav = document.getElementById("nav");
 	if (nav == null) {nav = document.getElementById("banner");}
 	if (nav != null) {nav.style.display = "block";}
+	
+	var main = document.getElementById("mainbox");
+	if (main != null) {main.style.marginTop = "40px";}
 }
