5eb51ca618c6a1850ad4e4cead58d9d98af7e240
Frank Dowsett <wixardy@adium.im>
2011-10-30 13:35:26 -0400

    Ask the user to login to Facebook when we get a bad password response. Fixes #15583

--- Plugins/Purple Service/AIFacebookXMPPAccount.m
+++ Plugins/Purple Service/AIFacebookXMPPAccount.m
@@ -203,13 +203,17 @@ enum {
 		[self requestFacebookAuthorization];
 
 	} else {
+		[self setValue:nil
+		   forProperty:@"mustPromptForPasswordOnNextConnect"
+				notify:NotifyNever];
 		[super passwordReturnedForConnect:inPassword returnCode:returnCode context:inContext];
 	}
 }
 
 - (void)retrievePasswordThenConnect
 {
-	if ([self boolValueForProperty:@"Prompt For Password On Next Connect"]) 
+	if ([self boolValueForProperty:@"Prompt For Password On Next Connect"] ||
+		[self boolValueForProperty:@"mustPromptForPasswordOnNextConnect"])
 		/* We attempted to connect, but we had incorrect authorization. Display our auth request window. */
 		[self requestFacebookAuthorization];
 
