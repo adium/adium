e01460814c85b4cccd897a845de9f27570776161
Patrick Steinhardt <ps@pks.im>
2011-11-14 16:24:44 +0100

    Fix user-scrolling in AIAutoScrollTextView. Fixes #15456.

--- Source/AIAutoScrollTextView.m
+++ Source/AIAutoScrollTextView.m
@@ -64,7 +64,6 @@
 		[self stopScrolling];
 	} else{
 		[self startScrolling];
-
     }
 }
 
@@ -72,7 +71,7 @@
 - (void)mouseExited:(NSEvent *)theEvent
 {
     if (!scrollTimer) {
-        [self startScrolling];
+		[self startScrolling];
     }
     
     [super mouseExited:theEvent];
@@ -80,11 +79,10 @@
 
 - (void)startScrolling
 {
+    [[self enclosingScrollView] setHasVerticalScroller:NO];
 	[[self enclosingScrollView] setLineScroll:0.0f];
 	[[self enclosingScrollView] setPageScroll:0.0f];
-	[[self enclosingScrollView] setVerticalScroller:nil];
-	[[self enclosingScrollView] setHasVerticalScroller:NO];
-	
+
 	// Start scrolling
 	scrollLocation = [[[self enclosingScrollView] contentView] bounds].origin.y;
 	maxScroll = [[self textStorage] size].height;
@@ -100,11 +98,16 @@
 {
 	[scrollTimer invalidate]; [scrollTimer release]; scrollTimer = nil;
 
-	// Enable scrolling and show the scrollbar
-	[[self enclosingScrollView] setLineScroll:10.0f];
-	[[self enclosingScrollView] setPageScroll:10.0f];
-	[[self enclosingScrollView] setVerticalScroller:[[[NSScroller alloc] init] autorelease]];
+	// Enable scrolling
     [[self enclosingScrollView] setHasVerticalScroller:YES];
+    [[self enclosingScrollView] setLineScroll:10.0f];
+	[[self enclosingScrollView] setPageScroll:10.0f];
+    
+    /* 
+     * Scroll to correct location, otherwise scrolling will start
+     * at the end of the last manual scroll
+     */
+    [[[self enclosingScrollView] contentView] scrollPoint:NSMakePoint(0, scrollLocation)];
 }
 
 // Scroll the credits
@@ -115,7 +118,7 @@
 	if (scrollLocation > maxScroll || scrollLocation < 0) {
 		scrollLocation = 0;
     }
-    
+
 	[self scrollPoint:NSMakePoint(0, scrollLocation)];
 }
 
