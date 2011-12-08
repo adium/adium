7e28af1484639561891907ba9abc8a895e04910f
Thijs Alkemade <me@thijsalkema.de>
2011-12-09 00:30:12 +0100

    Forgot to remove this.

--- Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.m
+++ Plugins/Dual Window Interface/ESDualWindowMessageAdvancedPreferences.m
@@ -201,11 +201,6 @@
 	NSMenu	*menu = [[NSMenu allocWithZone:[NSMenu menuZone]] initWithTitle:@""];
 	
 	//Generate all the available time stamp formats
-//    NSDateFormatter	*noSecondsNoAMPM = [NSDateFormatter localizedDateFormatterShowingSeconds:NO showingAMorPM:NO];
-//    NSDateFormatter	*noSecondsAMPM = [NSDateFormatter localizedDateFormatterShowingSeconds:NO showingAMorPM:YES];
-//    NSDateFormatter	*secondsNoAMPM = [NSDateFormatter localizedDateFormatterShowingSeconds:YES showingAMorPM:NO];
-//    NSDateFormatter	*secondsAMPM = [NSDateFormatter localizedDateFormatterShowingSeconds:YES showingAMorPM:YES];
-	
 	//If there is no difference between the time stamp with AM/PM and the one without, the localized time stamp must
 	//not include AM/PM.  Since these menu items would appear as duplicates we exclude them.
 	
