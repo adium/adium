3d233361cee29785034d17e4739a700710b18201
Frank Dowsett <wixardy@adium.im>
2011-12-18 22:14:51 -0500

    Redux: Messages.

--- Source/AIAdvancedPreferencesPlugin.m
+++ Source/AIAdvancedPreferencesPlugin.m
@@ -18,6 +18,7 @@
 #import "AIMessageAlertsAdvancedPreferences.h"
 #import "AIConfirmationsAdvancedPreferences.h"
 #import "AIWindowHandlingPreferences.h"
+#import "AIMessagePreferences.h"
 
 @implementation AIAdvancedPreferencesPlugin
 
@@ -27,6 +28,7 @@
 	messageAlertsPreferences = [(AIMessageAlertsAdvancedPreferences *)[AIMessageAlertsAdvancedPreferences preferencePane] retain];
 	confirmationsPreferences = [(AIConfirmationsAdvancedPreferences *)[AIConfirmationsAdvancedPreferences preferencePane] retain];
 	windowHandlingPreferences = [(AIWindowHandlingPreferences *)[AIWindowHandlingPreferences preferencePane] retain];
+	messagePreferences = [(AIMessagePreferences *)[AIMessagePreferences preferencePane] retain];
 }
 
 @end
