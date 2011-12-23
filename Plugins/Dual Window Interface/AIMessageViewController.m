44e198e40b2c22330dca833160b88326e26f962d
Frank Dowsett <wixardy@adium.im>
2011-12-23 16:12:23 -0500

    Correct the user list's size calculation by verifying the allowed width after the minimum is set. Fixes #15577

--- Plugins/Dual Window Interface/AIMessageViewController.m
+++ Plugins/Dual Window Interface/AIMessageViewController.m
@@ -1274,13 +1274,13 @@
 - (CGFloat)_userListViewDividerPositionIgnoringUserMinimum:(BOOL)ignoreUserMinimum
 {
 	CGFloat splitViewWidth = splitView_verticalSplit.frame.size.width;
-	CGFloat allowedWidth = (splitViewWidth / 2) - [splitView_verticalSplit dividerThickness];
+	CGFloat allowedWidth = AIfloor(splitViewWidth / 2) - [splitView_verticalSplit dividerThickness];
 	CGFloat width = ignoreUserMinimum ? USER_LIST_DEFAULT_WIDTH : userListMinWidth;
 	
+	if (width < USER_LIST_DEFAULT_WIDTH)
+		width = USER_LIST_DEFAULT_WIDTH;
 	if (width > allowedWidth)
 		width = allowedWidth;
-	else if (width < USER_LIST_DEFAULT_WIDTH)
-		width = USER_LIST_DEFAULT_WIDTH;
 	
 	if (userListOnRight)
 		return splitViewWidth - width;
