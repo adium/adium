fe86c9186fe36728a15bf3fba615e323d4facdce
Thijs Alkemade <me@thijsalkema.de>
2012-01-29 15:59:53 +0100

    Refactored AIListThemeWindowController to be acceptable to the static analyzer.

--- Source/AIListThemeWindowController.h
+++ Source/AIListThemeWindowController.h
@@ -98,7 +98,8 @@
 	NSString		*themeName;
 }
 
-+ (id)editListThemeWithName:(NSString *)inName onWindow:(NSWindow *)parentWindow notifyingTarget:(id)inTarget __attribute__((objc_method_family(new)));
+- (void)showOnWindow:(id)parentWindow __attribute__((ns_consumes_self));
+- (id)initWithName:(NSString *)inName notifyingTarget:(id)inTarget;
 - (IBAction)cancel:(id)sender;
 - (IBAction)okay:(id)sender;
 - (void)preferenceChanged:(id)sender;
