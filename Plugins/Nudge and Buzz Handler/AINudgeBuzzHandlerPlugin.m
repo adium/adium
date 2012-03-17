5243ffc76feeb6f3a6819254af8459b293f53027
Robert Vehse <robertvehse@fastmail.fm>
2012-03-17 22:31:44 +0100

    "Request Attention"

--- Plugins/Nudge and Buzz Handler/AINudgeBuzzHandlerPlugin.m
+++ Plugins/Nudge and Buzz Handler/AINudgeBuzzHandlerPlugin.m
@@ -34,7 +34,7 @@
 
 #import "AINudgeBuzzHandlerPlugin.h"
 
-#define NOTIFICATION				AILocalizedString(@"Request attention", "Request attention (nudge or buzz) menu item")
+#define NOTIFICATION				AILocalizedString(@"Request Attention", "Request Attention (nudge or buzz) menu item")
 #define TOOLBAR_NOTIFY_IDENTIFIER	@"NotifyParticipants"
 
 @interface AINudgeBuzzHandlerPlugin()
