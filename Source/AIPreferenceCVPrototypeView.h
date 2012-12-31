b7814142d9cfa5f561316313b52a46f7e0a24588
Frank Dowsett <wixardy@adium.im>
2012-12-31 11:50:05 -0500

    Synthesized properties need ivars.

--- Source/AIPreferenceCVPrototypeView.h
+++ Source/AIPreferenceCVPrototypeView.h
@@ -14,7 +14,9 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-@interface AIPreferenceCVPrototypeView : NSView
+@interface AIPreferenceCVPrototypeView : NSView {
+	NSCollectionViewItem *item;
+}
 
 @property (assign) NSCollectionViewItem *item;
 
