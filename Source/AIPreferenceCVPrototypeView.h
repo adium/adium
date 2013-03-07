709288e06f8de88699f4b5f8dcf5853644232dce
Thijs Alkemade <thijsalkemade@gmail.com>
2013-03-07 14:07:43 +0100

    http://hg.adium.im/adium/rev/82370a8e83bc was wrong, this should not have been weak, it generates a runtime error.
    I'm not sure if this is safe, but it's the only thing I can get to work.

--- Source/AIPreferenceCVPrototypeView.h
+++ Source/AIPreferenceCVPrototypeView.h
@@ -14,10 +14,8 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-@interface AIPreferenceCVPrototypeView : NSView {
-	__weak NSCollectionViewItem *item;
-}
+@interface AIPreferenceCVPrototypeView : NSView
 
-@property (weak) NSCollectionViewItem *item;
+@property (assign, nonatomic) NSCollectionViewItem *item;
 
 @end
