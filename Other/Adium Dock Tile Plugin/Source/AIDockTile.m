d06d26c8ed3a5868fb7c0b2f007c13f5a30ec1f8
Thijs Alkemade <me@thijsalkema.de>
2013-03-20 00:45:23 +0100

    Removed adding Icon\r file to Adium.app to set the dock icon image.
    * This breaks Sparkle's delta updates, which sucks, and isn't very multi-user friendly. Refs #14386
    * The icon now persists after quitting Adium using a dock tile plugin.
    * The plugin can provide a menu for when Adium is not running, for now only having the option "Launch in Debug Mode", but future ideas are "Launch And Log Me In With Status...", etc.
    * Added a --debug flag to Adium to automatically start it in debug mode.

---
+++ Other/Adium Dock Tile Plugin/Source/AIDockTile.m
@@ -0,0 +1,91 @@
+/*
+ * Adium is the legal property of its developers, whose names are listed in the copyright file included
+ * with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#import "AIDockTile.h"
+
+@implementation AIDockTile
+
+- (void)setDockTile:(NSDockTile *)inDockTile
+{
+	if (!inDockTile) return;
+	
+	CFPreferencesAppSynchronize(CFSTR("com.adiumX.adiumX"));
+	CFStringRef path = CFPreferencesCopyAppValue(CFSTR("DockTilePath"), CFSTR("com.adiumX.adiumX"));
+	
+	if (!path) {
+		NSLog(@"DockTilePath not found.");
+		return;
+	}
+	
+	NSImage *image = [[[NSImage alloc] initByReferencingFile:(NSString *)path] autorelease];
+	NSImageView *view = [[[NSImageView alloc] init] autorelease];
+	
+	[view setImage:image];
+	[inDockTile setContentView:view];
+	[inDockTile display];
+	
+	CFRelease(path);
+}
+
+- (void)launchDebug
+{
+	NSLog(@"Now pretend Adium is launching in debug mode");
+	
+	NSString *path = [[NSBundle bundleForClass:[self class]] bundlePath];
+	
+	NSMutableArray *pathComponents = [[[path pathComponents] mutableCopy] autorelease];
+	
+	// PlugIns/AIDockTilePlugin.docktileplugin
+	[pathComponents removeLastObject];
+	[pathComponents removeLastObject];
+	
+	NSString *adiumPath = [[[NSString pathWithComponents:pathComponents] stringByAppendingPathComponent:@"MacOS"] stringByAppendingPathComponent:@"Adium"];
+	
+	NSLog(@"Launching Adium from %@", adiumPath);
+	
+	NSTask *adiumTask = [[[NSTask alloc] init] autorelease];
+	
+	[adiumTask setLaunchPath:adiumPath];
+	[adiumTask setArguments:@[@"--debug"]];
+	
+	[adiumTask launch];
+}
+
+- (NSMenu *)dockMenu
+{
+	if (dockMenu) return dockMenu;
+	
+	dockMenu = [[NSMenu alloc] init];
+	NSMenuItem *menuItem = [[NSMenuItem allocWithZone:[NSMenu menuZone]] initWithTitle:@"Launch in Debug Mode"
+																				action:@selector(launchDebug)
+																		 keyEquivalent:@""];
+	menuItem.target = self;
+	
+	[dockMenu addItem:menuItem];
+	
+	[menuItem release];
+	
+	return dockMenu;
+}
+
+- (void)dealloc
+{
+	[dockMenu release];
+	
+	[super dealloc];
+}
+
+@end
--- Source/AILoginWindowController.m
+++ Source/AILoginWindowController.m
@@ -121,7 +121,7 @@
 #endif
 
 #ifndef DEBUG_BUILD
-	// If we're not in a debug build, activate debug logging if checked.
+	// If we're not in a debug build, activate debug logging if checked, or if the user passed --debug
 	if (checkBox_debugMode.state == NSOnState) {
 		AIEnableDebugLogging();
 	}
