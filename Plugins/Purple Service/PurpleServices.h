af5729af330b168aeb49803d3f312bcb9e9759e6
Robert Vehse <robertvehse@fastmail.fm>
2017-03-23 09:51:47 +0100

    Remove Facebook protocol code once more leaving JSONKit in this time.

--- Plugins/Purple Service/PurpleServices.h
+++ Plugins/Purple Service/PurpleServices.h
@@ -18,7 +18,6 @@
 #import "ESDotMacService.h"
 #import "AIMobileMeService.h"
 #import "ESICQService.h"
-#import "PurpleFacebookService.h"
 #import "ESGaduGaduService.h"
 #import "AIGTalkService.h"
 #import "ESIRCService.h"
